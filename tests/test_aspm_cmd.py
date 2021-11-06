80f8ca4e678a42d5d69b72ccb1e846fc3bff0c74
Antti Laakso <antti.laakso@intel.com>
2021-11-06 19:03:40 +0200

    tests: add tests for aspm command
    Add tests for pepc aspm info/set commands.
    
    Signed-off-by: Antti Laakso <antti.laakso@intel.com>

---
+++ tests/test_aspm_cmd.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+#
+# -*- coding: utf-8 -*-
+# vim: ts=4 sw=4 tw=100 et ai si
+#
+# Copyright (C) 2021 Intel Corporation
+# SPDX-License-Identifier: BSD-3-Clause
+#
+# Author: Antti Laakso <antti.laakso@linux.intel.com>
+
+"""Test module for 'pepc' project 'aspm' command."""
+
+from common import run_pepc
+
+def test_aspm_info():
+    """Test 'pepc aspm info' command."""
+    run_pepc("aspm info", exp_ret=0)
+
+def test_aspm_set():
+    """Test 'pepc aspm set' command."""
+
+    good_options = [
+        "",
+        "--policy",
+        "--policy default",
+        "--policy performance",
+        "--policy powersave",
+        "--policy powersupersave"]
+
+    for option in good_options:
+        run_pepc(f"aspm set {option}", exp_ret=0)
+
+    run_pepc("aspm set --policy badpolicyname", exp_ret=-1)
