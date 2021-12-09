ef9f85d09b448c31427d3722c6db31b17165e4ca
Artem Bityutskiy <artem.bityutskiy@linux.intel.com>
2021-12-09 15:54:18 +0200

    tests: adjust MSR test to 'regsize' removal
    Signed-off-by: Artem Bityutskiy <artem.bityutskiy@linux.intel.com>

--- tests/test_msr.py
+++ tests/test_msr.py
@@ -20,6 +20,9 @@ from pepclibs.msr import MSR
 _TEST_DATA_BYTES = random.randbytes(8)
 _TEST_DATA = int.from_bytes(_TEST_DATA_BYTES, byteorder="little")
 
+# Max. 64-bit integer.
+MAX64 = (1 << 64) - 1
+
 #pylint:disable=no-self-use
 
 @patch("builtins.open", new_callable=mock_open, read_data=_TEST_DATA_BYTES)
@@ -33,31 +36,26 @@ class TestMSR(unittest.TestCase):
         with MSR.MSR() as msr:
             for addr in (MSR.MSR_PM_ENABLE, MSR.MSR_MISC_FEATURE_CONTROL, MSR.MSR_HWP_REQUEST):
                 for cpu in (0, 1, 99):
-                    for regsize in (4, 8):
-                        mask = (1 << 8 * regsize) - 1
-
-                        res = msr.read(addr, regsize=regsize, cpu=cpu)
-                        m_open.assert_called_with(Path(f"/dev/cpu/{cpu}/msr"), ANY)
-                        m_open().seek.assert_called_with(addr)
-                        self.assertEqual(res, _TEST_DATA & mask)
+                    res = msr.read(addr, cpu=cpu)
+                    m_open.assert_called_with(Path(f"/dev/cpu/{cpu}/msr"), ANY)
+                    m_open().seek.assert_called_with(addr)
+                    self.assertEqual(res, _TEST_DATA & MAX64)
 
     def test_read_iter(self, m_open):
         """Test the 'read_iter()' method, and verify output."""
 
         with MSR.MSR() as msr:
             for addr in (MSR.MSR_PM_ENABLE, MSR.MSR_MISC_FEATURE_CONTROL, MSR.MSR_HWP_REQUEST):
-                for regsize in (4, 8):
-                    mask = (1 << 8 * regsize) - 1
-                    cpus = [0, 1, 3, 4]
+                cpus = [0, 1, 3, 4]
 
-                    for cpu, res in msr.read_iter(addr, regsize=regsize, cpus=cpus):
-                        m_open.assert_called_with(Path(f"/dev/cpu/{cpu}/msr"), ANY)
-                        m_open().seek.assert_called_with(addr)
-                        self.assertEqual(cpu, cpus.pop(0))
-                        self.assertEqual(res, _TEST_DATA & mask)
+                for cpu, res in msr.read_iter(addr, cpus=cpus):
+                    m_open.assert_called_with(Path(f"/dev/cpu/{cpu}/msr"), ANY)
+                    m_open().seek.assert_called_with(addr)
+                    self.assertEqual(cpu, cpus.pop(0))
+                    self.assertEqual(res, _TEST_DATA & MAX64)
 
-                    self.assertEqual(m_open().read.call_count, 4)
-                    m_open.reset_mock()
+                self.assertEqual(m_open().read.call_count, 4)
+                m_open.reset_mock()
 
     def test_write(self, m_open):
         """Test the 'write()' method, and verify call arguments."""
@@ -65,15 +63,12 @@ class TestMSR(unittest.TestCase):
         with MSR.MSR() as msr:
             for addr in (MSR.MSR_PM_ENABLE, MSR.MSR_MISC_FEATURE_CONTROL, MSR.MSR_HWP_REQUEST):
                 for cpu in (0, 1, 99):
-                    for regsize in (4, 8):
-                        mask = (1 << 8 * regsize) - 1
+                    msr.write(addr, _TEST_DATA & MAX64, cpus=cpu)
+                    m_open.assert_called_with(Path(f"/dev/cpu/{cpu}/msr"), ANY)
+                    m_open().seek.assert_called_with(addr)
 
-                        msr.write(addr, _TEST_DATA & mask, regsize=regsize, cpus=cpu)
-                        m_open.assert_called_with(Path(f"/dev/cpu/{cpu}/msr"), ANY)
-                        m_open().seek.assert_called_with(addr)
-
-                        ref_data = int.to_bytes(_TEST_DATA & mask, regsize, byteorder="little")
-                        m_open().write.assert_called_with(ref_data)
+                    ref_data = int.to_bytes(_TEST_DATA & MAX64, 8, byteorder="little")
+                    m_open().write.assert_called_with(ref_data)
 
     def test_set(self, m_open):
         """
@@ -84,22 +79,18 @@ class TestMSR(unittest.TestCase):
         with MSR.MSR() as msr:
             for addr in (MSR.MSR_PM_ENABLE, MSR.MSR_MISC_FEATURE_CONTROL, MSR.MSR_HWP_REQUEST):
                 for cpu in (0, 1, 99):
-                    for regsize in (4, 8):
-                        mask = (1 << 8 * regsize) - 1
+                    msr.set(addr, _TEST_DATA & MAX64, cpus=cpu)
+                    m_open().write.assert_not_called()
 
-                        msr.set(addr, _TEST_DATA & mask, regsize=regsize, cpus=cpu)
-                        m_open().write.assert_not_called()
+                    new_value = _TEST_DATA + 1
+                    msr.set(addr, new_value & MAX64, cpus=cpu)
 
-                        new_value = _TEST_DATA + 1
-                        msr.set(addr, new_value & mask, regsize=regsize, cpus=cpu)
+                    m_open().seek.assert_called_with(addr)
+                    m_open().write.assert_called_once()
 
-                        m_open().seek.assert_called_with(addr)
-                        m_open().write.assert_called_once()
-
-                        ref_data = int.to_bytes((_TEST_DATA | new_value) & mask, regsize,
-                                                byteorder="little")
-                        m_open().write.assert_called_with(ref_data)
-                        m_open.reset_mock()
+                    ref_data = int.to_bytes((_TEST_DATA | new_value) & MAX64, 8, byteorder="little")
+                    m_open().write.assert_called_with(ref_data)
+                    m_open.reset_mock()
 
     def test_clear(self, m_open):
         """
@@ -110,18 +101,15 @@ class TestMSR(unittest.TestCase):
         with MSR.MSR() as msr:
             for addr in (MSR.MSR_PM_ENABLE, MSR.MSR_MISC_FEATURE_CONTROL, MSR.MSR_HWP_REQUEST):
                 for cpu in (0, 1, 99):
-                    for regsize in (4, 8):
-                        mask = (1 << 8 * regsize) - 1
+                    msr.clear(addr, 0 & MAX64, cpus=cpu)
+                    m_open().write.assert_not_called()
 
-                        msr.clear(addr, 0 & mask, regsize=regsize, cpus=cpu)
-                        m_open().write.assert_not_called()
+                    msr.clear(addr, _TEST_DATA & MAX64, cpus=cpu)
+                    m_open().write.assert_called_once()
 
-                        msr.clear(addr, _TEST_DATA & mask, regsize=regsize, cpus=cpu)
-                        m_open().write.assert_called_once()
-
-                        ref_data = int.to_bytes(0 & mask, regsize, byteorder="little")
-                        m_open().write.assert_called_with(ref_data)
-                        m_open.reset_mock()
+                    ref_data = int.to_bytes(0 & MAX64, 8, byteorder="little")
+                    m_open().write.assert_called_with(ref_data)
+                    m_open.reset_mock()
 
     def test_toggle_bit(self, m_open):
         """
@@ -132,20 +120,16 @@ class TestMSR(unittest.TestCase):
         with MSR.MSR() as msr:
             for addr in (MSR.MSR_PM_ENABLE, MSR.MSR_MISC_FEATURE_CONTROL, MSR.MSR_HWP_REQUEST):
                 for cpu in (0, 1, 99):
-                    for regsize in (4, 8):
-                        mask = (1 << 8 * regsize) - 1
+                    val = int(bool(_TEST_DATA & 1))
+                    msr.toggle_bit(addr, 0, val, cpus=cpu)
+                    m_open().write.assert_not_called()
 
-                        val = int(bool(_TEST_DATA & 1))
-                        msr.toggle_bit(addr, 0, val, regsize=regsize, cpus=cpu)
-                        m_open().write.assert_not_called()
+                    msr.toggle_bit(addr, 0, int(not bool(val)), cpus=cpu)
+                    m_open().write.assert_called_once()
 
-                        msr.toggle_bit(addr, 0, int(not bool(val)), regsize=regsize, cpus=cpu)
-                        m_open().write.assert_called_once()
-
-                        ref_data = int.to_bytes((_TEST_DATA ^ 1) & mask, regsize,
-                                                byteorder="little")
-                        m_open().write.assert_called_with(ref_data)
-                        m_open.reset_mock()
+                    ref_data = int.to_bytes((_TEST_DATA ^ 1) & MAX64, 8, byteorder="little")
+                    m_open().write.assert_called_with(ref_data)
+                    m_open.reset_mock()
 
 if __name__ == '__main__':
     unittest.main()
