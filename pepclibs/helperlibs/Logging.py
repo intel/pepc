d10f98768c3be736e2c47ab289d8bc0f3fd4ee1f
Artem Bityutskiy <artem.bityutskiy@linux.intel.com>
2021-12-13 11:59:28 +0200

    Logging: change debug messages prefix
    This patch change the debug messages prefix. The main change is that now it
    also includes time since epoch, which is useful for debugging. The 'DEBUG:'
    part was removed, in order to keep the messages shorter.
    
    This patch also reworks the code a bit.
    * Docstrings improvement
    * The 'MyFormatter' class constructor now accepts the 'prefix_debug' argument,
      which can be used for setting any prefix for debug messages separately.
    * Removed a bunch of unused attributes from 'MyFormatter'.
    
    Signed-off-by: Artem Bityutskiy <artem.bityutskiy@linux.intel.com>

--- pepclibs/helperlibs/Logging.py
+++ pepclibs/helperlibs/Logging.py
@@ -32,6 +32,9 @@ ERROR = logging.ERROR
 ERRINFO = logging.ERROR + 1
 CRITICAL = logging.CRITICAL
 
+# The default prefix for debug messages.
+_DEFAULT_DBG_PREFIX="[%(created)f] [%(asctime)s] [%(module)s,%(lineno)d]"
+
 def _error_traceback(logger, msgformat, *args):
     """Print an error message occurred along with the traceback."""
 
@@ -99,14 +102,18 @@ class _MyFormatter(logging.Formatter):
     """
 
     # pylint: disable=protected-access
-    def __init__(self, prefix=None, colors=None):
+    def __init__(self, prefix=None, prefix_debug=None,
+                 colors=None):
         """
         The constructor. The arguments are as follows.
-          * prefix' - the prefix for all non-info and non-debug messages. Default is no prefix. Info
-                      messages come without any formating, debug messages have a prefix including
-                      the time-stamp, module name, and line number.
-          * colors - a dictionary containing colorama color codes for message time-staps and
-                     prefixes.
+          * prefix - prefix for non-info and non-debug messages (info messages go without any
+                    formating, debug message prefix is controlled with 'prerix_debug'). By default,
+                    the prefix is just the log level name.
+          * prefix_debug - prefix for debug messages. The default value is '_DEFAULT_DBG_PREFIX'.
+                           See the 'logging' module documentation for the prefix format.
+          * colors - a dictionary containing colorama color codes to use for 'prefix' and
+                     'prefix_debug'.
+
         """
 
         def _start(level):
@@ -121,25 +128,33 @@ class _MyFormatter(logging.Formatter):
 
         logging.Formatter.__init__(self, "%(levelname)s: %(message)s", "%H:%M:%S")
 
-        if not prefix:
-            prefix = ""
-        self._prefix = prefix
-        self._orig_fmt = self._style._fmt
-
         if not colors or not colorama:
             colors = {}
-
         self.myfmt = {}
+
+        if not prefix:
+            prefix = ""
+        if prefix:
+            prefix += ": "
 
         for lvl, pfx in ((WARNING, "warning"), (ERROR, "error"), (CRITICAL, "critical error"),
                          (NOTICE, "notice")):
-            if not self._prefix:
+            if not prefix:
                 pfx = pfx.title()
-            self.myfmt[lvl] = _start(lvl) + self._prefix + pfx + _end(lvl) + ": %(message)s"
-        # Prefix debug messages with a green-colored time-stamp, module name and line number.
+            self.myfmt[lvl] = _start(lvl) + prefix + pfx + _end(lvl) + ": %(message)s"
+
+        # Debug messages formatting.
         lvl = DEBUG
-        self.myfmt[lvl] = "[" + _start(lvl) + "%(asctime)s" + _end(lvl) + \
-                          "] [%(module)s,%(lineno)d] " + self._style._fmt
+        if prefix_debug is None:
+            prefix_debug = _DEFAULT_DBG_PREFIX
+        if not prefix_debug:
+            prefix_debug = ""
+        else:
+            prefix_debug += ": "
+        self.myfmt[lvl] = prefix_debug + "%(message)s"
+        self.myfmt[lvl] = self.myfmt[lvl].replace("[", "[" + _start(lvl))
+        self.myfmt[lvl] = self.myfmt[lvl].replace("]", _end(lvl) + "]")
+
         # Leave the info messages without any formatting.
         self.myfmt[ERRINFO] = self.myfmt[INFO] = "%(message)s"
 
@@ -172,8 +187,8 @@ def setup_logger(prefix=None, loglevel=None, colored=None, info_stream=sys.stdou
                  error_stream=sys.stderr, info_logfile=None, error_logfile=None):
     """
     Setup and return a logger.
-      * prefix - usually the program name, but can be any prefix that will be used for "WARNING",
-                 "ERROR" and "CRITICAL" log level messages. No prefix is used by default.
+      * prefix - usually the program name, but can be any prefix that will be used for 'NOTICE',
+                 'WARNING', 'ERROR' and 'CRITICAL' log level messages. No prefix is used by default.
       * loglevel - the default log level. If not provided, this function initializes it depending on
                    the '-d' and '-q' command line options.
       * colored - whether the output should be colored or not. By default this function
@@ -188,9 +203,6 @@ def setup_logger(prefix=None, loglevel=None, colored=None, info_stream=sys.stdou
                        both.
       * error_logfile - same as 'info_logfile", but  will be used for all other logging levels.
     """
-
-    if prefix:
-        prefix = f"{prefix}: "
 
     if not loglevel:
         # Change log level names.
