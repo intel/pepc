f99c1ac982453bc1b0e33f49d4b23ebe93627fc3
Antti Laakso <antti.laakso@intel.com>
2022-01-11 11:55:18 +0200

    EmulProcs: add run() and run_verify() methods
    Both methods will be extended later on.
    
    Signed-off-by: Antti Laakso <antti.laakso@intel.com>

--- pepclibs/helperlibs/EmulProcs.py
+++ pepclibs/helperlibs/EmulProcs.py
@@ -11,12 +11,35 @@
 """Emulated version or the 'Procs' module for testing purposes."""
 
 import io
+from pepclibs.helperlibs._Common import ProcResult
+from pepclibs.helperlibs.Exceptions import ErrorNotSupported
 
 class EmulProc():
     """
     Emulated version of the 'Proc' class in the 'pepclibs.helperlibs.Procs' module. The class is
     used for testing purposes.
     """
+
+    def _get_cmd_result(self, cmd):
+        """Return pre-defined value for the command 'cmd'."""
+
+        if cmd not in self._cmds:
+            raise ErrorNotSupported(f"unsupported command: '{cmd}'")
+
+        return self._cmds[cmd]
+
+    def run_verify(self, cmd, **kwargs): # pylint: disable=unused-argument
+        """
+        Emulates 'Proc.run_verify()' for pre-defined set of commands. If command is not known,
+        raises an 'ErrorNotSupported' exception.
+        """
+
+        return (self._get_cmd_result(cmd), "")
+
+    def run(self, cmd, **kwargs): # pylint: disable=unused-argument
+        """Same as 'run_verify()', but emulates the 'Proc.run()' command."""
+
+        return ProcResult(stdout=self._get_cmd_result(cmd), stderr="", exitcode=0)
 
     def open(self, path, mode):
         """Create and return emulated file object."""
@@ -36,3 +59,6 @@ class EmulProc():
 
         # Opened files.
         self._files = {}
+
+        # Supported commands for 'run()' and 'run_verify()'.
+        self._cmds = {}
