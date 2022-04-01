5201fa6617c34d03b261ff081a620056be51dc74
Artem Bityutskiy <artem.bityutskiy@linux.intel.com>
2022-04-01 13:25:06 +0300

    New module: ProcessManager
    Add a new module which provides an easy to use 'get_pman()' function which
    creates the process manager of the right type depending on the host name. We
    have several copies of 'get_path()', but let's have it in one common place
    instead.
    
    Signed-off-by: Artem Bityutskiy <artem.bityutskiy@linux.intel.com>

---
+++ pepclibs/helperlibs/ProcessManager.py
@@ -0,0 +1,81 @@
+# -*- coding: utf-8 -*-
+# vim: ts=4 sw=4 tw=100 et ai si
+#
+# Copyright (C) 2020-2021 Intel Corporation
+# SPDX-License-Identifier: BSD-3-Clause
+#
+# Author: Artem Bityutskiy <artem.bityutskiy@linux.intel.com>
+
+"""
+This module provides a unified way of creating a process manager object for local or remote hosts.
+"""
+
+from pepclibs.helperlibs.Exceptions import Error
+
+def _check_for_none(hostname, **kwargs):
+    """
+    A helper function that makes sure that argument 'kwargs' are all set to 'None'.
+    """
+
+    for name, val in kwargs.items():
+        if val is not None:
+            raise Error(f"BUG: get_pmon: hostname is '{hostname}', but argument '{name}' is not "
+                        f"'None'. Instead, it is '{val}'")
+
+def get_pman(hostname, username=None, privkeypath=None, timeout=None, datapath=None):
+    """
+    Creates and returns a process manager object for host 'hostname'. At the moment there are
+    basically 3 possibilities.
+     1. 'hostname' is "localhost".
+        1.1. If 'username' is 'None', then create and return a 'LocalProcessManager' object, which
+             provides an effecient way of managing local processes.
+        1.2 If 'username' is not 'None', thenn create and return an 'SSHProcessManager' object,
+            which will connect to local host over SSH and manage processes over a local SSH
+            connectoin. This is a less effecient way of managing processes, but may be useful for,
+            say, debugging purposes.
+     2. 'hostname' is any string except for "localhost" and "emulation". Create and return an
+        'SSHProcessManager' object, which will be connected to the 'hostname' host over SSH and will
+        manage processes on the 'hostname' host over SSH.
+     3. 'hostname is "emulation". Create and return an 'EmulProcessManager' object. This object
+        is used for testing purposes only. However, if 'datapath' is 'None', then this is treated as
+        case #2.
+
+    The arguments are as follows.
+      * hostname - the host name to create a process manager object for.
+      * username - the user name to use for logging into the 'hostname' host over SSH.
+      * privkeypath - path to SSH private key to use for logging into the host.
+      * timeout - the SSH connection time out in seconds.
+      * datapath - path to the emulation data.
+
+    The 'hostname' argument is used for all types of process managers. The 'username',
+    'privkeypath', and 'timeout' arguments are used only for 'SSHProcessManager'. Have to be 'None'
+    for everything else. The 'datapath' argument is used only for the 'EmulProcessManager' process
+    manager, and it defines the emulated host data. Must be 'None' for everything else.
+    """
+
+    # pylint: disable=import-outside-toplevel
+
+    if hostname == "localhost" and username is None:
+        _check_for_none(hostname, username=username, privkeypath=privkeypath, timeout=timeout,
+                        datapath=datapath)
+
+        from pepclibs.helperlibs import LocalProcessManager
+
+        pman = LocalProcessManager.LocalProcessManager()
+    elif hostname == "emulation" and datapath is not None:
+        _check_for_none(hostname, username=username, privkeypath=privkeypath, timeout=timeout)
+
+        from pepclibs.helperlibs import EmulProcessManager
+
+        pman = EmulProcessManager.EmulProcessManager()
+        pman.init_testdata("CPUInfo", datapath)
+
+    else:
+        _check_for_none(hostname, datapath=datapath)
+
+        from pepclibs.helperlibs import SSHProcessManager
+
+        pman = SSHProcessManager.SSHProcessManager(hostname=hostname, username=username,
+                                                   privkeypath=privkeypath, timeout=timeout)
+
+    return pman
