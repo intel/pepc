intel/pepc

commit 0d6c8baac23cc76c69f3027358002f5567248e24

PCStateConfigCt: minor optimization
Form the error message when the error actually occurs, not in advance.

Signed-off-by: Artem Bityutskiy <artem.bityutskiy@linux.intel.com>

pepclibs/msr/PCStateConfigCtl.py
@@ -153,11 +153,6 @@ class PCStateConfigCtl(_FeaturedMSR.FeaturedMSR):
 
         for cpu, code in self._msr.read_bits(self.regaddr, finfo["bits"], cpus=cpus):
             if code not in finfo["rvals"]:
-                known_codes = ", ".join([str(cde) for cde in finfo["rvals"]])
-                msg = f"unexpected package C-state limit code '{code}' read from " \
-                      f"'{self.regname}' MSR ({self.regaddr}){self._proc.hostmsg}, known codes " \
-                      f"are: {known_codes}"
-
                 # No exact match. The limit is the closest lower known number. For example, if the
                 # known numbers are 0(PC0), 2(PC6), and 7(unlimited), and 'code' is 3, then the
                 # limit is PC6.
@@ -165,10 +160,11 @@ class PCStateConfigCtl(_FeaturedMSR.FeaturedMSR):
                     if cde <= code:
                         code = cde
                         break
-                else:
-                    raise Error(msg)
 
-                _LOG.debug(msg)
+                known_codes = ", ".join([str(cde) for cde in finfo["rvals"]])
+                raise Error(f"unexpected package C-state limit code '{code}' read from "
+                            f"'{self.regname}' MSR ({self.regaddr}){self._proc.hostmsg}, known "
+                            f"codes are: {known_codes}")
 
             res = {"pkg_cstate_limit" : finfo["rvals"][code],
                    "pkg_cstate_limits" : list(finfo["vals"].keys()),

commit 3051cfd6b88fbfc286cfe65f74b864c3a1a0d692

PCStateConfigCtl: bugfix: fix crash on unexpected limits
This patch fixes a regression introduced in

  0d6c8ba PCStateConfigCt: minor optimization

Th 'else' statement was incorrectly removed, so the the tool crashes whenever
an undefind PC-state limit is found, instead of falling back to the nearest
limit.

Signed-off-by: Artem Bityutskiy <artem.bityutskiy@intel.com>

pepclibs/msr/PCStateConfigCtl.py
@@ -168,11 +168,11 @@ class PCStateConfigCtl(_FeaturedMSR.FeaturedMSR):
                     if cde <= code:
                         code = cde
                         break
-
-                known_codes = ", ".join([str(cde) for cde in finfo["rvals"]])
-                raise Error(f"unexpected package C-state limit code '{code}' read from "
-                            f"'{self.regname}' MSR ({self.regaddr}){self._proc.hostmsg}, known "
-                            f"codes are: {known_codes}")
+                else:
+                    known_codes = ", ".join([str(cde) for cde in finfo["rvals"]])
+                    raise Error(f"unexpected package C-state limit code '{code}' read from "
+                                f"'{self.regname}' MSR ({self.regaddr}) on CPU {cpu}"
+                                f"{self._proc.hostmsg}, known codes are: {known_codes}")
 
             res = {"pkg_cstate_limit" : finfo["rvals"][code],
                    "pkg_cstate_limits" : list(finfo["vals"].keys()),