8d62c952b451b4316b8431196c90defab8c6f7cc
Artem Bityutskiy <artem.bityutskiy@linux.intel.com>
2022-04-28 11:10:57 +0300

    FSHelpers: remove unused 'set_default_perm()'
    Signed-off-by: Artem Bityutskiy <artem.bityutskiy@linux.intel.com>

--- pepclibs/helperlibs/FSHelpers.py
+++ pepclibs/helperlibs/FSHelpers.py
@@ -10,75 +10,12 @@
 This module contains misc. helper functions related to file-system operations.
 """
 
-import os
-import stat
 import time
-import shutil
 import logging
-from pathlib import Path
 from pepclibs.helperlibs import ProcessManager, Human
-from pepclibs.helperlibs.Exceptions import Error, ErrorExists
+from pepclibs.helperlibs.Exceptions import Error
 
 _LOG = logging.getLogger()
-
-def set_default_perm(path):
-    """
-    Set access mode for a 'path'. Mode is 666 for file and 777 for directory, and current umask
-    value is first masked out.
-    """
-
-    try:
-        curmode = os.stat(path).st_mode
-        # umask() returns existing umask, but requires new mask as an argument. Restore original
-        # mask immediately.
-        curumask = os.umask(0o022)
-        os.umask(curumask)
-
-        if stat.S_ISDIR(curmode):
-            mode = 0o0777
-        else:
-            mode = 0o0666
-
-        mode = ~curumask & mode
-        if stat.S_IMODE(curmode) != mode:
-            os.chmod(path, mode)
-    except OSError as err:
-        raise Error(f"cannot change '{path}' permissions to {oct(mode)}:\n{err}") from None
-
-def _copy_dir(src: Path, dst: Path, ignore=None):
-    """Implements the 'copy_dir()' function."""
-
-    try:
-        if not dst.parent.exists():
-            dst.parent.mkdir(parents=True)
-
-        if src.resolve() in dst.resolve().parents:
-            raise Error(f"cannot do recursive copy from '{src}' to '{dst}'")
-
-        ignore_names = None
-        if ignore:
-            ignore_names = lambda path, content: ignore
-
-        shutil.copytree(src, dst, ignore=ignore_names)
-    except (OSError, shutil.Error) as err:
-        raise Error(f"cannot copy '{src}' to '{dst}':\n{err}") from err
-
-def copy_dir(src: Path, dst: Path, exist_ok: bool = False, ignore=None):
-    """
-    Copy 'src' directory to 'dst'. The 'ignore' argument is a list of file or directory
-    names which will be ignored and not copied.
-    """
-
-    exists_err = f"cannot copy '{src}' to '{dst}', the destination path already exists"
-    if dst.exists():
-        if exist_ok:
-            return
-        raise ErrorExists(exists_err)
-
-    if not src.is_dir():
-        raise Error("cannot copy '{src}' to '{dst}', the destination path is not directory.")
-
-    _copy_dir(src, dst, ignore)
 
 def wait_for_a_file(path, interval=1, timeout=60, pman=None):
     """
