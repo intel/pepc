ab189babad52e4fcc0b178db20794b38e8804443
Artem Bityutskiy <artem.bityutskiy@linux.intel.com>
2022-04-28 14:30:41 +0300

    Dmesg: remove the unused 'default' argument
    There are not users for it.
    
    Signed-off-by: Artem Bityutskiy <artem.bityutskiy@linux.intel.com>

--- pepclibs/helperlibs/Dmesg.py
+++ pepclibs/helperlibs/Dmesg.py
@@ -15,18 +15,14 @@ This module provides convenient API for running Linux 'dmesg' tool.
 import itertools
 import difflib
 import logging
-from pepclibs.helperlibs.Exceptions import Error
 from pepclibs.helperlibs import LocalProcessManager, ClassHelpers
 
 _LOG = logging.getLogger()
 
-# A unique object used as the default value for the 'default' key in some functions.
-_RAISE = object()
-
 class Dmesg:
     """This class provides convenient API to the 'dmesg' tool."""
 
-    def run(self, join=True, strip=False, capture=False, default=_RAISE):
+    def run(self, join=True, strip=False, capture=False):
         """
         Execute the 'dmesg' program and return the output. The arguments are as follows.
           * join - if 'True', return the output as a single string, otherwise return it as a list of
@@ -34,16 +30,9 @@ class Dmesg:
           * strip - whether the output should be stripped from the trailing newlines.
           * capture - save the output in the 'captured' attribute in order to use it for generating
                       diffs (see 'get_new_messages()').
-          * default - the value to return in case of error (by default an exception is raised).
         """
 
-        try:
-            output = self._pman.run_verify("dmesg", join=False)[0]
-        except Error as err:
-            if default is _RAISE:
-                raise
-            _LOG.debug(err)
-            return default
+        output = self._pman.run_verify("dmesg", join=False)[0]
 
         if capture:
             self.captured = output
@@ -59,19 +48,16 @@ class Dmesg:
 
         return output
 
-    def get_new_messages(self, join=True, strip=False, default=_RAISE):
+    def get_new_messages(self, join=True, strip=False):
         """
-        Runs 'dmesg', compares the output to the last captured dmesg output, and returns the new
-        lines (those ptresent in the most recent dmesg output but absent in the captured dmesg
-        output). The arguments are the same as in 'run()'.
+        Run the 'dmesg' program, compare the output to the previously captured 'dmesg' output, and
+        return the new lines (those present in the most recent 'dmesg' output but absent from the
+        previously captured 'dmesg' output). The arguments are the same as in 'run()'.
         """
 
-        new_output = self.run(join=False, strip=False, capture=False, default=default)
-        if new_output is default:
-            return default
+        new_output = self.run(join=False, strip=False, capture=False)
 
         new_lines = []
-
         diff = difflib.unified_diff(self.captured, new_output, n=0,
                                     fromfile=f"{self._pman.hostname}-dmesg-before",
                                     tofile=f"{self._pman.hostname}-dmesg-after")
