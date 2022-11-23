ed19fb0fc7334fa8c834f065be51496f755705bb
Antti Laakso <antti.laakso@intel.com>
2022-11-23 11:28:27 +0200

    tests: add PStates save and restore test
    Add tests for 'pepc pstates save' and
    'pepc pstates restore' commands.
    
    Signed-off-by: Antti Laakso <antti.laakso@intel.com>

--- tests/test_pstates_cmd.py
+++ tests/test_pstates_cmd.py
@@ -10,15 +10,16 @@
 
 """Test module for 'pepc' project 'pstates' command."""
 
+import copy
 import pytest
 from common import get_pman, run_pepc, build_params
 from pcstates_common import is_prop_supported
 from pepclibs.helperlibs.Exceptions import Error
-from pepclibs.helperlibs import Human
+from pepclibs.helperlibs import Human, YAML
 from pepclibs import CPUInfo, PStates
 
 @pytest.fixture(name="params", scope="module")
-def get_params(hostspec):
+def get_params(hostspec, tmp_path_factory):
     """Yield a dictionary with information we need for testing."""
 
     emul_modules = ["CPUInfo", "PStates", "Systemctl"]
@@ -27,6 +28,7 @@ def get_params(hostspec):
          CPUInfo.CPUInfo(pman=pman) as cpuinfo, \
          PStates.PStates(pman=pman, cpuinfo=cpuinfo) as psobj:
         params = build_params(pman)
+        params["tmp_path"] = tmp_path_factory.mktemp(params["hostname"])
 
         params["cpus"] = cpuinfo.get_cpus()
         params["packages"] = cpuinfo.get_packages()
@@ -215,3 +217,57 @@ def test_pstates_config(params):
 
     _test_pstates_config_good(params)
     _test_pstates_config_bad(params)
+
+def test_pstates_save_restore(params):
+    """Test 'pepc pstates save' and 'pepc pstates restore' commands."""
+
+    pman = params["pman"]
+    hostname = params["hostname"]
+    tmp_path = params["tmp_path"]
+    scope_options = _get_scope_options(params)
+
+    good_options = [
+        "",
+        f"-o {tmp_path}/pstates.{hostname}"]
+
+    for option in good_options:
+        for scope in scope_options["good"]:
+            run_pepc(f"pstates save {option} {scope}", pman)
+
+        for scope in scope_options["bad"]:
+            run_pepc(f"pstates save {option} {scope}", pman, exp_exc=Error)
+
+    state_path = tmp_path / f"state.{hostname}"
+    run_pepc(f"pstates save -o {state_path}", pman)
+    state = YAML.load(state_path)
+
+    state_modified = copy.deepcopy(state)
+    for pname, pinfos in state_modified.items():
+        for pinfo in pinfos:
+            if pname in ("min_freq", "max_freq"):
+                pinfo["value"] = state["min_freq"][0]["value"]
+
+            if pname.endswith("_uncore_freq"):
+                pinfo["value"] = state["min_uncore_freq"][0]["value"]
+
+            if pname == "epb":
+                pinfo["value"] = int((pinfo["value"] + 1) / 2)
+
+            if pinfo["value"] == "on":
+                pinfo["value"] = "off"
+            elif pinfo["value"] == "off":
+                pinfo["value"] = "on"
+
+    state_modified_path = tmp_path / f"state_modified.{hostname}"
+    YAML.dump(state_modified, state_modified_path)
+    run_pepc(f"pstates restore -f {state_modified_path}", pman)
+
+    state_read_back_path = tmp_path / f"state_read_back.{hostname}"
+    run_pepc(f"pstates save -o {state_read_back_path}", pman)
+    read_back = YAML.load(state_read_back_path)
+    assert read_back == state_modified, "restoring P-states configuration failed"
+
+    run_pepc(f"pstates restore -f {state_path}", pman)
+    run_pepc(f"pstates save -o {state_read_back_path}", pman)
+    read_back = YAML.load(state_read_back_path)
+    assert read_back == state, "restoring P-states configuration failed"
