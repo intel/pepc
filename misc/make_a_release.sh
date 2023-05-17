12e7d2a65fada922ba55a5861f7aecebe76557dd
Artem Bityutskiy <artem.bityutskiy@linux.intel.com>
2023-05-17 16:36:41 +0300

    make_a_release.sh: remove svos remote suggestions
    Signed-off-by: Artem Bityutskiy <artem.bityutskiy@linux.intel.com>

--- misc/make_a_release.sh
+++ misc/make_a_release.sh
@@ -141,7 +141,7 @@ To finish the release:
 The commands would be:
 EOF
 
-for remote in "origin" "upstream" "public" "svos"; do
+for remote in "origin" "upstream" "public"; do
     echo "git push $remote $tag_name"
     if [ "$current_branch" = "main" ]; then
         echo "git push $remote main:main"
@@ -155,7 +155,7 @@ if [ "$current_branch" != "main" ]; then
     echo
     echo "Then merge the release branch back to main, and run the following commands:"
 
-    for remote in "origin" "upstream" "public" "svos"; do
+    for remote in "origin" "upstream" "public"; do
         echo "git push $remote main:main"
     done
 fi
