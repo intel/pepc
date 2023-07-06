intel/pepc

commit fc8e7ca49f5f25ba7cd8ee27c8a3f6ef061afb25

CPUInfo: rename hybrid CPU topology dictionary keys
"atom" -> "ecore"
"core" -> "pcore"

E-core/P-core are more commonly used when referring to Atom/Core on hybrid CPUs.

Signed-off-by: Niklas Neronin <niklas.neronin@intel.com>

pepclibs/CPUInfo.py
@@ -1075,7 +1075,7 @@ class CPUInfo(ClassHelpers.SimpleCloseContext):
 
     def get_hybrid_cpu_topology(self):
         """
-        Returns Atom/Core CPU list on hybrid CPUs, otherwise returns 'None'.
+        Returns P-core/E-core CPU list on hybrid CPUs, otherwise returns 'None'.
         If the kernel does not support hybrid CPU topology, this function will return 'None'
         """
 
@@ -1084,8 +1084,8 @@ class CPUInfo(ClassHelpers.SimpleCloseContext):
 
         if not self._hybrid_cpus:
             self._hybrid_cpus = {}
-            for arch in ("atom", "core"):
-                self._hybrid_cpus[arch] = self._read_range(f"/sys/devices/cpu_{arch}/cpus")
+            for arch, name in {"atom" : "ecore", "core" : "pcore"}:
+                self._hybrid_cpus[name] = self._read_range(f"/sys/devices/cpu_{arch}/cpus")
 
         return self._hybrid_cpus
 
@@ -1180,7 +1180,7 @@ class CPUInfo(ClassHelpers.SimpleCloseContext):
         self._all_cpus = None
         # Set of online CPUs.
         self._cpus = None
-        # Dictionary of Atom/Core CPUs.
+        # Dictionary of P-core/E-core CPUs.
         self._hybrid_cpus = None
 
         # General CPU information.

pepctool/_PepcTopology.py
@@ -95,7 +95,7 @@ def topology_info_command(args, pman):
                 headers.append("Hybrid")
                 fmt += "    %6s"
 
-                performance_cores = set(cpuinfo.get_hybrid_cpu_topology()["core"])
+                performance_cores = set(cpuinfo.get_hybrid_cpu_topology()["pcore"])
                 for tline in topology:
                     if tline["CPU"] in performance_cores:
                         tline["hybrid"] = "P-core"

commit 511b5c9ab9c57a7bcb8e5568b9296d4e37bff593

CPUInfo: fix 'get_hybrid_cpu_topology()' exception
Trying to incorrectly unpack dictionary raised an exception, this caused
'pepc' to crash on hybrid CPUs.

Replacing the dictionary with a tuple of tuples fixes the issue.

This crash was introduced in:
fc8e7ca  CPUInfo: rename hybrid CPU topology dictionary keys

Signed-off-by: Niklas Neronin <niklas.neronin@intel.com>

pepclibs/CPUInfo.py
@@ -1084,7 +1084,7 @@ class CPUInfo(ClassHelpers.SimpleCloseContext):
 
         if not self._hybrid_cpus:
             self._hybrid_cpus = {}
-            for arch, name in {"atom" : "ecore", "core" : "pcore"}:
+            for arch, name in (("atom", "ecore"), ("core", "pcore")):
                 self._hybrid_cpus[name] = self._read_range(f"/sys/devices/cpu_{arch}/cpus")
 
         return self._hybrid_cpus