intel/pepc

commit 8d935bcb2570dfee7b24b87a9a489476ae28d554

_Pepc: preserve options order
Print properties in the same order as they were requested via command line.

Signed-off-by: Artem Bityutskiy <artem.bityutskiy@linux.intel.com>

pepctool/_PepcCStates.py
@@ -22,12 +22,11 @@ _LOG = logging.getLogger()
 def cstates_info_command(args, pman):
     """Implements the 'cstates info' command."""
 
-    # Options to print.
-    pnames = []
-
-    for optname in CStates.PROPS:
-        if getattr(args, f"{optname}"):
-            pnames.append(optname)
+    # The options to print.
+    if not hasattr(args, "oargs"):
+        pnames = "all"
+    else:
+        pnames = list(getattr(args, "oargs"))
 
     # The output format to use.
     fmt = "yaml" if args.yaml else "human"

pepctool/_Pepc.py
@@ -178,9 +178,14 @@ def _add_info_subcommand_options(props, subpars):
     """
 
     for name, pinfo in props.items():
-        text = _get_info_subcommand_prop_help_text(pinfo)
+        kwargs = {}
+        kwargs["default"] = argparse.SUPPRESS
+        kwargs["nargs"] = 0
+        kwargs["help"] = _get_info_subcommand_prop_help_text(pinfo)
+        kwargs["action"] = ArgParse.OrderedArg
+
         option = f"--{name.replace('_', '-')}"
-        subpars.add_argument(option, action="store_true", help=text)
+        subpars.add_argument(option, **kwargs)
 
 def _get_config_subcommand_prop_help_text(pinfo):
     """

pepctool/_PepcPStates.py
@@ -22,12 +22,11 @@ _LOG = logging.getLogger()
 def pstates_info_command(args, pman):
     """Implements the 'pstates info' command."""
 
-    # Options to print.
-    pnames = []
-
-    for optname in PStates.PROPS:
-        if getattr(args, optname):
-            pnames.append(optname)
+    # The options to print.
+    if not hasattr(args, "oargs"):
+        pnames = "all"
+    else:
+        pnames = list(getattr(args, "oargs"))
 
     # The output format to use.
     fmt = "yaml" if args.yaml else "human"

pepctool/_PepcPower.py
@@ -22,13 +22,11 @@ _LOG = logging.getLogger()
 def power_info_command(args, pman):
     """Implements the 'power info' command."""
 
-    # Options to print.
-    pnames = []
-
-    for optname in Power.PROPS:
-        if getattr(args, f"{optname}"):
-            pnames.append(optname)
-
+    # The options to print.
+    if not hasattr(args, "oargs"):
+        pnames = "all"
+    else:
+        pnames = list(getattr(args, "oargs"))
     # The output format to use.
     fmt = "yaml" if args.yaml else "human"
 

commit a369a7397e01d66eeba8955719283ecda55a9842

_PepcCStates: fix printing C-states infotmation
Fix regression introduced in

	8d935bc _Pepc: preserve options order

The C-states enable/disable status is not printed because the
'cstates_info_command()' functions uses 'None' for indicating all properties,
not "all".

Signed-off-by: Artem Bityutskiy <artem.bityutskiy@linux.intel.com>

pepctool/_PepcCStates.py
@@ -24,7 +24,7 @@ def cstates_info_command(args, pman):
 
     # The options to print.
     if not hasattr(args, "oargs"):
-        pnames = "all"
+        pnames = None
     else:
         pnames = list(getattr(args, "oargs"))
 