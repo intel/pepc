f89b37569a84765e531bcbc26ce6e346451871fc
Artem Bityutskiy <artem.bityutskiy@linux.intel.com>
2023-07-26 12:44:35 +0300

    _PepcPrinter: add a helper function
    Separate out the aggregate C-states information dictionary creation to a
    function: '_build_aggr_rcsinfo()'. Makes the code easier to follow.
    
    Signed-off-by: Artem Bityutskiy <artem.bityutskiy@linux.intel.com>

--- pepctool/_PepcPrinter.py
+++ pepctool/_PepcPrinter.py
@@ -237,6 +237,42 @@ class _PropsPrinter(ClassHelpers.SimpleCloseContext):
 
         return aggr_pinfo
 
+    def _build_aggr_rcsinfo(self, csnames, cpus, spnames="all"):
+        """
+        Build the aggregate requestable C-states information dictionary. The arguments are as follows.
+          * csnames - C-state names to print information about (all C-states by default).
+          * cpus - CPU numbers to read and print C-state information for (all CPUs by default).
+          * spnames - names of sub-properties of the 'pname' property that should also be printed.
+                      Value "all" will include all sub-properties and value 'None' won't include
+                      any.
+
+        This method is similar to '_build_aggr_pinfo()' and returns a dictionary of a similar structure.
+        """
+
+        aggr_rcsinfo = {}
+        # C-states info 'csinfo' has the following format:
+        #
+        # { "POLL" : {"disable" : True, "latency" : 0, "residency" : 0, ... },
+        #   "C1E"  : {"disable" : False, "latency" : 2, "residency" : 1, ... },
+        #   ... }
+        for cpu, csinfo in self._pobj.get_cstates_info(csnames=csnames, cpus=cpus):
+            for pname, values in csinfo.items():
+                if pname not in aggr_rcsinfo:
+                    aggr_rcsinfo[pname] = {}
+
+                for name, val in values.items():
+                    if name not in spnames or val is None:
+                        continue
+
+                    if name not in aggr_rcsinfo[pname]:
+                        aggr_rcsinfo[pname][name] = {val : [cpu]}
+                    elif val not in aggr_rcsinfo[pname][name]:
+                        aggr_rcsinfo[pname][name][val] = [cpu]
+                    else:
+                        aggr_rcsinfo[pname][name][val].append(cpu)
+
+        return aggr_rcsinfo
+
     def _normalize_pnames(self, pnames, skip_ro=False):
         """Validate property names in 'pnames' and return a normalized list."""
 
@@ -499,28 +535,8 @@ class CStatesPrinter(_PropsPrinter):
 
         group = csnames == "all"
 
-        aggr_rcsinfo = {}
         try:
-            # C-states info 'csinfo' has the following format:
-            #
-            # { "POLL" : {"disable" : True, "latency" : 0, "residency" : 0, ... },
-            #   "C1E"  : {"disable" : False, "latency" : 2, "residency" : 1, ... },
-            #   ... }
-            for cpu, csinfo in self._pobj.get_cstates_info(csnames=csnames, cpus=cpus):
-                for pname, values in csinfo.items():
-                    if pname not in aggr_rcsinfo:
-                        aggr_rcsinfo[pname] = {}
-
-                    for name, val in values.items():
-                        if name not in spnames or val is None:
-                            continue
-
-                        if name not in aggr_rcsinfo[pname]:
-                            aggr_rcsinfo[pname][name] = {val : [cpu]}
-                        elif val not in aggr_rcsinfo[pname][name]:
-                            aggr_rcsinfo[pname][name][val] = [cpu]
-                        else:
-                            aggr_rcsinfo[pname][name][val].append(cpu)
+            aggr_rcsinfo = self._build_aggr_rcsinfo(csnames, cpus, spnames=spnames)
         except ErrorNotSupported as err:
             _LOG.warning(err)
             _LOG.info("C-states are not supported")
