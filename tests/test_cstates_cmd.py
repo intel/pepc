11565001ce2dd546defe82fc7df3d0e34bd8a4ac
Artem Bityutskiy <artem.bityutskiy@linux.intel.com>
2023-10-28 13:51:13 +0300

    tests/test_cstates_cmd: refactoring
    Make this test look more similar to 'test_pstates_cmd'.
    
    Signed-off-by: Artem Bityutskiy <artem.bityutskiy@linux.intel.com>

--- tests/test_cstates_cmd.py
+++ tests/test_cstates_cmd.py
@@ -68,48 +68,90 @@ def test_cstates_info(params):
     # Treat the target system as Sapphire Rapids Xeon.
     common.run_pepc("cstates info --override-cpu-model 0x8F", pman)
 
-def test_cstates_config(params):
-    """Test 'pepc cstates config' command."""
+def _get_good_config_opts(params, sname="package"):
+    """Return good options for testing 'pepc pstates config'."""
 
     cpu = 0
-    pman = params["pman"]
+    opts = []
     pobj = params["pobj"]
 
-    good = []
+    if sname == "global":
+        if pobj.prop_is_supported("governor", cpu):
+            opts += ["--governor"]
+            for governor in pobj.get_cpu_prop("governors", cpu)["val"]:
+                opts += [f"--governor {governor}"]
+        return opts
+
     if pobj.prop_is_supported("idle_driver", cpu):
-        good += ["--enable all",
-                         "--disable all",
-                         f"--enable {params['cstates'][-1]}",
-                         f"--disable {params['cstates'][-1]}"]
+        opts += ["--enable all",
+                 "--disable all",
+                f"--enable {params['cstates'][-1]}",
+                f"--disable {params['cstates'][-1]}"]
 
     if pobj.prop_is_supported("pkg_cstate_limit", cpu):
-        good += ["--pkg-cstate-limit"]
+        opts += ["--pkg-cstate-limit"]
         lock = pobj.get_cpu_prop("pkg_cstate_limit_lock", cpu)["val"]
         if lock == "off":
             limit = pobj.get_cpu_prop("pkg_cstate_limit", cpu)["val"]
-            good += [f"--pkg-cstate-limit {limit.upper()}", f"--pkg-cstate-limit {limit.lower()}"]
-    if pobj.prop_is_supported("c1_demotion", cpu):
-        good += ["--c1-demotion", "--c1-demotion on", "--c1-demotion OFF"]
-    if pobj.prop_is_supported("c1_undemotion", cpu):
-        good += ["--c1-undemotion", "--c1-undemotion on", "--c1-undemotion OFF"]
-    if pobj.prop_is_supported("c1e_autopromote", cpu):
-        good += ["--c1e-autopromote", "--c1e-autopromote on", "--c1e-autopromote OFF"]
-    if pobj.prop_is_supported("cstate_prewake", cpu):
-        good += ["--cstate-prewake", "--cstate-prewake on", "--cstate-prewake OFF"]
+            opts += [f"--pkg-cstate-limit {limit.upper()}",
+                     f"--pkg-cstate-limit {limit.lower()}"]
 
-    for opt in good:
+    if pobj.prop_is_supported("c1_demotion", cpu):
+        opts += ["--c1-demotion",
+                 "--c1-demotion on",
+                 "--c1-demotion OFF"]
+
+    if pobj.prop_is_supported("c1_undemotion", cpu):
+        opts += ["--c1-undemotion",
+                 "--c1-undemotion on",
+                 "--c1-undemotion OFF"]
+
+    if pobj.prop_is_supported("c1e_autopromote", cpu):
+        opts += ["--c1e-autopromote",
+                 "--c1e-autopromote on",
+                 "--c1e-autopromote OFF"]
+
+    if pobj.prop_is_supported("cstate_prewake", cpu):
+        opts += ["--cstate-prewake",
+                "--cstate-prewake on",
+                "--cstate-prewake OFF"]
+    return opts
+
+def _get_bad_config_opts():
+    """Return bad options for testing 'pepc pstates config'."""
+
+    opts = ["--enable CC0",
+            "--disable CC0",
+            "--cstate-prewake meh",
+            "--governor reardenmetal"]
+
+    return opts
+
+def test_cstates_config_good(params):
+    """Test 'pepc cstates config' command with bad options."""
+
+    pman = params["pman"]
+
+    for opt in _get_good_config_opts(params, sname="package"):
         for cpunum_opt in props_common.get_good_cpunum_opts(params, sname="package"):
             common.run_pepc(f"cstates config {opt} {cpunum_opt}", pman)
 
         for cpunum_opt in props_common.get_bad_cpunum_opts(params):
             common.run_pepc(f"cstates config {opt} {cpunum_opt}", pman, exp_exc=Error)
 
-    bad = [
-        "--enable CC0",
-        "--disable CC0"
-        "--cstate-prewake meh"]
+    for opt in _get_good_config_opts(params, sname="global"):
+        for cpunum_opt in props_common.get_good_cpunum_opts(params, sname="global"):
+            common.run_pepc(f"cstates config {opt} {cpunum_opt}", pman)
 
-    for opt in bad:
+        for cpunum_opt in props_common.get_bad_cpunum_opts(params):
+            common.run_pepc(f"cstates config {opt} {cpunum_opt}", pman, exp_exc=Error)
+
+def test_cstates_config_bad(params):
+    """Test 'pepc cstates config' command with bad options."""
+
+    pman = params["pman"]
+
+    for opt in _get_bad_config_opts():
         common.run_pepc(f"cstates config {opt}", pman, exp_exc=Error)
 
         for cpunum_opt in props_common.get_good_cpunum_opts(params, sname="package"):
@@ -117,21 +159,6 @@ def test_cstates_config(params):
 
         for cpunum_opt in props_common.get_bad_cpunum_opts(params):
             common.run_pepc(f"cstates config {opt} {cpunum_opt}", pman, exp_exc=Error)
-
-    # Test without "cpunum" options.
-    good = []
-    bad = []
-
-    if pobj.prop_is_supported("governor", cpu):
-        good += ["--governor"]
-        for governor in pobj.get_cpu_prop("governors", cpu)["val"]:
-            good += [f"--governor {governor}"]
-        bad += ["--governor reardenmetal"]
-
-    for opt in good:
-        common.run_pepc(f"cstates config {opt}", pman)
-    for opt in bad:
-        common.run_pepc(f"cstates config {opt}", pman, exp_exc=Error)
 
 def test_cstates_save_restore(params):
     """Test 'pepc cstates save' and 'pepc cstates restore' commands."""
