a16af6cb97b708e8349ec43e942173a9a5a4326b
Artem Bityutskiy <artem.bityutskiy@linux.intel.com>
2024-01-01 19:34:48 +0200

    _SysfsIO: simplify code by removing 'bypass_cache'
    Reasons to remove 'bypass_cache'.
    1. Not used except for 'write_verify()', which can be implemented without
       'bypass_cache'.
    2. Code is simpler.
    3. Consistency: somewhat analogous 'MSR.py' does not have 'bypass_cache'.
    
    Signed-off-by: Artem Bityutskiy <artem.bityutskiy@linux.intel.com>

--- pepclibs/_SysfsIO.py
+++ pepclibs/_SysfsIO.py
@@ -82,35 +82,19 @@ class SysfsIO(ClassHelpers.SimpleCloseContext):
         with contextlib.suppress(KeyError):
             del self._cache[path]
 
-    def read(self, path, bypass_cache=False, what=None):
+    def read(self, path, what=None):
         """
         Read a sysfs file at 'path'. The arguments are as follows.
           * path - path of the sysfs file to read.
-          * bypass_cache - if 'False', use the cache, if 'False', do not use the cache.
           * what - short description of the file at 'path', will be included to the exception
                    message in case of a failure.
-
-        If 'bypass_cache' is True, remove the potentially cached value from the cash, read directly
-        from the sysfs file, and do not add the read value to the cache.
-
-        If 'bypass_cache' is 'False', return the value from the cache is possible, otherwise read
-        from the sysfs file and add the result to the cache. If cache is disabled, skip all the
-        cache operations.
 
         Return the contents of the file at 'path'. Raise 'ErrorNotSupported' if the file does not
         exist.
         """
 
-        if bypass_cache:
-            self.cache_remove(path)
-        else:
-            with contextlib.suppress(ErrorNotFound):
-                return self.cache_get(path)
-
-        if self._enable_cache and path in self._cache:
-            if not bypass_cache:
-                return self._cache[path]
-            del self._cache[path]
+        with contextlib.suppress(ErrorNotFound):
+            return self.cache_get(path)
 
         try:
             with self._pman.open(path, "r") as fobj:
@@ -125,27 +109,22 @@ class SysfsIO(ClassHelpers.SimpleCloseContext):
             raise ErrorNotSupported(f"failed to read{what} from '{path}'{self._pman.hostmsg}\n"
                                     f"{err.indent(2)}") from err
 
-        if not bypass_cache:
-            self.cache_add(path, val)
-        return val
+        return self.cache_add(path, val)
 
-    def read_int(self, path, bypass_cache=False, what=None):
+    def read_int(self, path, what=None):
         """
         Read a sysfs file 'path' and return its contents as an interger value. The arguments are as
         follows.
           * path - path of the sysfs file to read.
-          * bypass_cache - if 'False', use the cache, if 'False', do not use the cache.
           * what - short description of the file at 'path', will be included to the exception
                    message in case of a failure.
 
         Validate the contents of the 'path', raise 'Error' if the contents is not an integer value.
         Otherwise, return the the value as an integer ('int' type). Return 'None' if the file does
         not exist.
-
-        Refer 'read()' for more information about 'bypass_cache'.
         """
 
-        val = self.read(path, bypass_cache=bypass_cache, what=what)
+        val = self.read(path, what=what)
 
         try:
             return Trivial.str_to_int(val, what=what)
@@ -154,17 +133,13 @@ class SysfsIO(ClassHelpers.SimpleCloseContext):
             raise Error(f"bad contents of{what} sysfs file '{path}'{self._pman.hostmsg}\n"
                         f"{err.indent(2)}") from err
 
-    def write(self, path, val, bypass_cache=False, what=None):
+    def write(self, path, val, what=None):
         """
         Write value 'val' to a sysfs file at 'path'. The arguments are as follows.
           * path - path of the sysfs file to write to.
           * val - the value to write.
-          * bypass_cache - if 'False', use the cache, if 'False', do not use the cache.
           * what - short description of the file at 'path', will be included to the exception
                    message in case of a failure.
-
-        If 'bypass_cache' is 'True', write to the sysfs file and clear the cache for path 'path'
-        (remove the possibly cached value). If 'bypass_cache' is 'False', cache the written value.
         """
 
         self.cache_remove(path)
@@ -188,35 +163,29 @@ class SysfsIO(ClassHelpers.SimpleCloseContext):
             raise ErrorNotSupported(f"failed to write value '{val}' to{what} sysfs file '{path}'"
                                     f"{self._pman.hostmsg}:\n{err.indent(2)}") from err
 
-        if self._enable_cache and not bypass_cache:
-            self._cache[path] = val
+        self.cache_add(path, val)
 
-    def write_verify(self, path, val, bypass_cache=False, what=None, retries=0, sleep=0):
+    def write_verify(self, path, val, what=None, retries=0, sleep=0):
         """
         Write value 'val' to a sysfs file at 'path' and verify that it was "accepted" by the kernel
         by reading it back and comparing to the written value. The arguments are as follows.
           * path - path of the sysfs file to write to.
           * val - the value to write.
-          * bypass_cache - if 'False', use the cache, if 'False', do not use the cache.
           * what - short description of the file at 'path', will be included to the exception
                    message in case of a failure.
           * retries - how many times to re-try the verification.
           * sleep - sleep for 'sleep' amount of seconds before repeating the verification.
 
         Raise 'ErrorVerifyFailed' if the value read was not the same as value written.
-
-        If 'bypass_cache' is 'True', write to the sysfs file and clear the cache for path 'path'
-        (remove the possibly cached value). If 'bypass_cache' is 'False', cache the written value.
         """
 
-        self.write(path, val, bypass_cache=True, what=what)
+        self.write(path, val, what=what)
 
         while True:
             # Read CPU frequency back and verify that it was set correctly.
-            new_val = self.read(path, bypass_cache=True, what=what)
+            self.cache_remove(path)
+            new_val = self.read(path, what=what)
             if val == new_val:
-                if not bypass_cache:
-                    self.cache_add(path, new_val)
                 return new_val
 
             retries -= 1
