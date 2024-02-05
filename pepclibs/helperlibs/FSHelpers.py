af4147789d70f71433ac6d47ed50b538389c3003
Artem Bityutskiy <artem.bityutskiy@linux.intel.com>
2024-02-05 09:14:42 +0200

    FSHelpers: add 'mount_debugfs()' and 'get_mount_points()'
    Add a couple of helpers for mounting file-systems. These helpers are moved here
    from the 'wult' project.
    
    Signed-off-by: Artem Bityutskiy <artem.bityutskiy@linux.intel.com>

--- pepclibs/helperlibs/FSHelpers.py
+++ pepclibs/helperlibs/FSHelpers.py
@@ -10,12 +10,78 @@
 This module contains misc. helper functions related to file-system operations.
 """
 
+import os
 import time
 import logging
+from pathlib import Path
+from collections import namedtuple
 from pepclibs.helperlibs import ProcessManager, Human
 from pepclibs.helperlibs.Exceptions import Error
 
 _LOG = logging.getLogger()
+
+# The debugfs mount point path.
+DEBUGFS_MOUNT_POINT = Path("/sys/kernel/debug")
+
+def get_mount_points(pman=None):
+    """
+    Parse mount points in '/proc/mounts' for each mount point yield the following named tuple.
+      * device - name of the mounted device.
+      * mntpoint - mount point path.
+      * fstype - file-system type.
+      * options - list of options.
+
+    The arguments are as follows.
+      * pman - the process manager object defining the host to parse '/proc/mounts' on (local host
+               by default).
+    """
+
+    mounts_file = "/proc/mounts"
+    mntinfo = namedtuple("mntinfo", ["device", "mntpoint", "fstype", "options"])
+
+    with ProcessManager.pman_or_local(pman) as wpman:
+        with wpman.open(mounts_file, "r") as fobj:
+            try:
+                contents = fobj.read()
+            except OSError as err:
+                msg = Error(err).indent(2)
+                raise Error(f"cannot read '{mounts_file}':\n{msg}") from err
+
+    for line in contents.splitlines():
+        if not line:
+            continue
+
+        device, mntpoint, fstype, options, _ = line.split(maxsplit=4)
+        yield mntinfo(device, mntpoint, fstype, options.split(","))
+
+def mount_debugfs(mnt=None, pman=None):
+    """
+    Mount the debugfs file-system. The arguments are as follow.
+      * mnt - the mount point path (default is 'DEBUGFS_MOUNT_POINT').
+      * pman - the process manager object defining the host to mount debugfs on (local host by
+               default).
+
+    Return a tuple of the following elements.
+      * the mount point path.
+      * 'True' if debugfs was mounted by this function, 'False' it has already been mounted.
+    """
+
+    if not mnt:
+        mnt = DEBUGFS_MOUNT_POINT
+    else:
+        try:
+            mnt = Path(os.path.realpath(mnt)).resolve()
+        except OSError as err:
+            msg = Error(err).indent(2)
+            raise Error(f"cannot resolve path '{mnt}':\n{msg}") from None
+
+    for mntinfo in get_mount_points(pman=pman):
+        if mntinfo.fstype == "debugfs" and Path(mntinfo.mntpoint) == mnt:
+            # Already mounted.
+            return mnt, False
+
+    pman.run_verify(f"mount -t debugfs none '{mnt}'")
+    return mnt, True
 
 def wait_for_a_file(path, interval=1, timeout=60, pman=None):
     """
