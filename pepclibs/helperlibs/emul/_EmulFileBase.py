dc42eb6d2b168ee68b9096c2e7a4692207f696b6
Adam Hawley <adam.james.hawley@intel.com>
2024-02-08 15:26:12 +0200

    _EmulFileBase: fix docstrings
    Signed-off-by: Adam Hawley <adam.james.hawley@intel.com>

--- pepclibs/helperlibs/emul/_EmulFileBase.py
+++ pepclibs/helperlibs/emul/_EmulFileBase.py
@@ -7,19 +7,18 @@
 # Authors: Antti Laakso <antti.laakso@linux.intel.com>
 #          Niklas Neronin <niklas.neronin@intel.com>
 
-"""Provide the base class for sub-classes emulateing sysfs, procfs, and debugfs files."""
+"""Provide the base class for sub-classes emulating sysfs, procfs, and debugfs files."""
 
 class EmulFileBase:
-    """Base class for sub-classes emulateing sysfs, procfs, and debugfs files."""
+    """Base class for sub-classes emulating sysfs, procfs, and debugfs files."""
 
     def open(self, mode):
         """Create a file in the temporary directory and return the file object with 'mode'."""
-
         raise NotImplementedError()
 
     def __init__(self, path):
         """
         Class constructor. Arguments are as follows:
+         * path - emulated path of the file.
         """
-
         self.path = path
