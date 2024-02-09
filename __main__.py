9086372a5363eff5851d2b0317d2b7f089d05533
Artem Bityutskiy <artem.bityutskiy@linux.intel.com>
2024-02-09 10:21:08 +0200

    pepc: ensure data are available when standalone
    When pepc is uses as a stanalone zipapp program, the TPMI data are not
    available, and 'tpmi' commands fail. Solve this issue by extracting TPMI data
    to a temporary directory.
    
    Signed-off-by: Artem Bityutskiy <artem.bityutskiy@linux.intel.com>

--- __main__.py
+++ __main__.py
@@ -8,8 +8,37 @@
 #
 # Author: Antti Laakso <antti.laakso@intel.com>
 
+import os
 import sys
+import zipfile
+import tempfile
+from pathlib import Path
+from pepclibs.helperlibs import ProjectFiles
 from pepctool._Pepc import main
 
 if __name__ == "__main__":
-    sys.exit(main())
+    basepath = Path(__file__).parent
+
+    # If the parent is not a regular file, not a zipapp archive.
+    if not basepath.is_file():
+        sys.exit(main())
+
+    # This is a zipapp archive. It includeds the python code and the data. In order for the data to
+    # be found by the tool, it should be extracted somewhere and made accessible to the too via the
+    # data path environment variable.
+
+    envvar = ProjectFiles.get_project_data_envvar("pepc")
+    if envvar in os.environ:
+        # The environment variable is already set, assume the test data are there, nothing to do.
+        sys.exit(main())
+
+    # Create a temporary directory and extract tool data there.
+    zipapp_path = Path(__file__).parent
+    with zipfile.ZipFile(zipapp_path) as zf, tempfile.TemporaryDirectory() as tmpdir:
+        # Unzip the data sub-directory.
+        for path in zf.namelist():
+            if path.startswith("tpmi/"):
+                zf.extract(path, tmpdir)
+
+        os.environ[envvar] = tmpdir
+        sys.exit(main())
