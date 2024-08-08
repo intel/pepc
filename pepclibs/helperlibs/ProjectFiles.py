722664986a423b8cb6b0016b131e6590ec56c0a0
Adam Hawley <adam.james.hawley@intel.com>
2024-08-08 16:45:53 +0300

    ProjectFiles: fix an issue where environment variables are ignored
    Fix an issue where if the helpers path environment variable was
    overwritten in the shell running the current process, the variable will
    be overwritten by checking the value of the variable using the local
    process manager.
    
    Signed-off-by: Adam Hawley <adam.james.hawley@intel.com>

--- pepclibs/helperlibs/ProjectFiles.py
+++ pepclibs/helperlibs/ProjectFiles.py
@@ -168,11 +168,13 @@ def find_project_helper(prjname, helper, pman=None):
     with ProcessManager.pman_or_local(pman) as wpman:
         paths = [Path(sys.argv[0]).parent]
 
+        path = None
+        envvar = get_project_helpers_envvar(prjname)
         # First check to see if the environment variable is specified on 'wpman' before also
         # checking localhost.
-        envvar = get_project_helpers_envvar(prjname)
-        stdout, _ = wpman.run_verify(f"echo ${envvar}")
-        path = stdout.strip()
+        if wpman.is_remote:
+            stdout, _ = wpman.run_verify(f"echo ${envvar}")
+            path = stdout.strip()
         if not path:
             path = os.environ.get(envvar)
 
