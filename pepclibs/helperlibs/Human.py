intel/pepc

commit 917a631c5ba23b681465520933ed6ae10557bfe4

Human: fix 'ms' being confused for 'Ms' in 'parse_human()'
Fix a bug where 'parse_human()' will scale values with unit 'ms' as if
they had the 'Ms' unit.

Signed-off-by: Adam Hawley <adam.james.hawley@intel.com>

pepclibs/helperlibs/Human.py
@@ -262,7 +262,7 @@ def _tokenize(hval, specs, name=None, multiple=True):
     tokens = {}
     rest = hval.lower()
     for spec in specs:
-        split = rest.split(spec.lower(), 1)
+        split = rest.split(spec, 1)
         if len(split) > 1:
             tokens[spec] = split[0]
             rest = split[1]
@@ -330,13 +330,13 @@ def _tokenize_prepare(unit):
                 specs[spec] = spec
             scalers[spec] = _BYTESIZE_SCALERS[pfx]
     elif unit == "s":
-        # For time, allow for day/hour/minute specifiers.
-        specs["d"] = "day"
-        specs["h"] = "hour"
-        specs["m"] = "minute"
-        scalers["d"] = 24 * 60 * 60
-        scalers["h"] = 60 * 60
-        scalers["m"] = 60
+        # For time, allow for day/hour/minute specifiers in upper and lower case.
+        specs["d"] = specs["D"] = "day"
+        specs["h"] = specs["H"] = "hour"
+        specs["m"] = specs["M"] = "minute"
+        scalers["d"] = scalers["D"] = 24 * 60 * 60
+        scalers["h"] = scalers["H"] = 60 * 60
+        scalers["m"] = scalers["M"] = 60
         # Allow for multiple specifiers for time, like in "1d 5h".
         multiple = True
 

commit 5c581904817bd816e003bae2beeae4046d9cbe4a

Human: Fix parsing Hz values by `parse_human()`
Commit 917a631c5ba2 ("Human: fix 'ms' being confused for 'Ms' in
'parse_human()'") removed converting specs to lowercase to distuingish
'ms' from 'Ms', but if we don't get rid of similar conversion on `hval`,
it will miss all of the '*Hz` units.

Fixes: 917a631c5ba2 ("Human: fix 'ms' being confused for 'Ms' in 'parse_human()'"

Signed-off-by: Patryk Wlazlyn <patryk.wlazlyn@intel.com>

pepclibs/helperlibs/Human.py
@@ -260,7 +260,7 @@ def _tokenize(hval, specs, name=None, multiple=True):
         name = ""
 
     tokens = {}
-    rest = hval.lower()
+    rest = hval
     for spec in specs:
         split = rest.split(spec, 1)
         if len(split) > 1: