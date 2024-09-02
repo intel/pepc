521bc7d3506f3d018d7d4744a4a7e268121be562
Artem Bityutskiy <artem.bityutskiy@linux.intel.com>
2024-09-02 18:58:13 +0300

    LsPCI: remove dependency on the 'lspci' tool
    Stop using the 'lspci' tool, because there is no real need in it. At the moment
    none of the advanced fatures of 'lspci' is used, so just get the basic
    information directly from sysfs.
    
    Signed-off-by: Artem Bityutskiy <artem.bityutskiy@linux.intel.com>

--- pepclibs/LsPCI.py
+++ pepclibs/LsPCI.py
@@ -6,82 +6,52 @@
 #
 # Author: Antti Laakso <antti.laakso@intel.com>
 
-"""This module can be used to get information about PCI devices in system."""
+"""List the PCI devices."""
 
-import re
+from pathlib import Path
 from pepclibs.helperlibs.Exceptions import Error
 from pepclibs.helperlibs import LocalProcessManager, ClassHelpers, Trivial
 
 class LsPCI(ClassHelpers.SimpleCloseContext):
-    """This is a wrapper class for 'lspci' tool."""
+    """List PCI devices."""
 
-    @staticmethod
-    def _parse_dev_info(lines):
-        """
-        Parse 'lspci' output lines in 'lines'. The lines should belong to a single device and should
-        not be stripped. Returns the resulting dictionary.
-        """
+    def _get_info(self, devaddr):
+        """Get basic information about a PCI device."""
 
-        line = lines[0].strip().split()
-        info = {"pciaddr" : line[0]}
+        info = {"pciaddr" : devaddr}
 
-        # Find two 4 digit hex numbers with a colon in between, all inside of square brackets.
-        x = re.findall(r"\[([0-9a-fA-F]{4}:[0-9a-fA-F]{4})\]", lines[0])
-        if x:
-            line = x[0].split(":")
+        basepath = self._sysfs_base / devaddr
 
-            what = f"Vendor ID for PCI device {info['pciaddr']}"
-            info["vendorid"] = Trivial.str_to_int(line[0], base=16, what=what)
+        for key, descr, fname in (("devid", "Vendor ID", "device"),
+                                  ("vendorid", "Device DI", "vendor")):
+            path = basepath / fname
+            try:
+                with self._pman.open(path, "r") as fobj:
+                    val = fobj.read()
+            except Error as err:
+                raise type(err)(f"sysfs file read operation failed{self._pman.hostmsg}:\n"
+                                f"{err.indent(2)}") from err
 
-            what = f"Device ID for PCI device {info['pciaddr']}"
-            info["devid"] = Trivial.str_to_int(line[1], base=16, what=what)
+            what = f"{descr} for PCI device {devaddr}"
+            info[key] = Trivial.str_to_int(val, base=16, what=what)
 
         return info
 
     def get_info(self, devaddr):
         """
-        Return dictionary of PCI device information. Argument 'devaddr' is in format of:
-        [[[[<domain>]:]<bus>]:][<slot>][.[<func>]].
+        Get basic information about a PCI device. The arguments are as follows.
+          * devaddr - the PCI device address (n the [<domain>:<bus>:<slot>.<func> format]).
+
+        Return the device information dictionary.
         """
 
-        cmd = f"{self._lspci_path} -D -nn -vv -s {devaddr}"
-        stdout, _ = self._pman.run_verify(cmd, join=False)
-        if not stdout:
-            raise Error(f"failed to get information for PCI slot: {devaddr}")
-
-        return self._parse_dev_info(stdout)
+        return self._get_info(devaddr)
 
     def get_devices(self):
-        """Generator yields device info as dictionary for each device. """
+        """Yield device info as dictionary for every PCI device on the system."""
 
-        cmd = f"{self._lspci_path} -D -nn -vv"
-        stdout, _ = self._pman.run_verify(cmd, join=False)
-
-        # The output structure is as follows:
-        #
-        # 0000:02:00.0 0300: 102b:0522 (rev 05) (prog-if 00 [VGA controller])
-        #    DeviceName: ServerEngines Pilot III
-        #    ....
-        # 0000:04:00.0 0200: 8086:1533 (rev 03)
-        #    DeviceName: Intel i210
-        #    ....
-        #
-        # So every line without a space at the beginning is a marker of a new device. Use this
-        # property to split the output on per-device chunks.
-        lines = []
-        for line in stdout:
-            if not line.strip():
-                continue
-
-            if re.match(r"\s", line):
-                lines.append(line)
-            else:
-                if lines:
-                    yield self._parse_dev_info(lines)
-                lines = [line]
-
-        if lines:
-            yield self._parse_dev_info(lines)
+        for devaddr, _, _ in self._pman.lsdir(self._sysfs_base):
+            yield self._get_info(devaddr)
 
     def __init__(self, pman=None):
         """
@@ -90,7 +60,7 @@ class LsPCI(ClassHelpers.SimpleCloseContext):
         """
 
         self._pman = pman
-        self._lspci_path = "lspci"
+        self._sysfs_base = Path("/sys/bus/pci/devices")
 
         self._close_pman = pman is None
 
