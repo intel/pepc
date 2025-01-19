67466ad3bfd16ce8de71193d8bd1b0a48f51feff
Artem Bityutskiy <artem.bityutskiy@linux.intel.com>
2025-01-19 09:29:44 +0200

    Trivial: refactor and annotate types
    Refactor the module to match the new style. Annotate types.
    
    Signed-off-by: Artem Bityutskiy <artem.bityutskiy@linux.intel.com>

--- pepclibs/helperlibs/Trivial.py
+++ pepclibs/helperlibs/Trivial.py
@@ -1,56 +1,74 @@
 # -*- coding: utf-8 -*-
 # vim: ts=4 sw=4 tw=100 et ai si
 #
-# Copyright (C) 2020-2021 Intel Corporation
+# Copyright (C) 2020-2025 Intel Corporation
 # SPDX-License-Identifier: BSD-3-Clause
 #
 # Author: Artem Bityutskiy <artem.bityutskiy@linux.intel.com>
 
 """
-This module contains common trivial helpers.
+Common trivial helpers.
 """
 
+from __future__ import annotations # Remove when switching to Python 3.10+.
 import os
 import pwd
 from pepclibs.helperlibs.Exceptions import Error
 
-def is_root():
+def is_root() -> bool:
     """
-    Return 'True' if current process has superuser (root) privileges and 'False' otherwise.
+    Check if the current process has superuser (root) privileges.
+
+    Returns:
+        bool: True if the current process has superuser privileges, False otherwise.
     """
 
     try:
         return os.getuid() == 0 or os.geteuid() == 0
     except OSError as err:
         msg = Error(err).indent(2)
-        raise Error(f"failed to get process UID:\n{msg}") from None
+        raise Error(f"Failed to get process UID:\n{msg}") from None
 
-def get_pid():
-    """Return current process ID."""
+def get_pid() -> int:
+    """
+    Return the current process ID.
+
+    Returns:
+        int: The current process ID.
+    """
 
     try:
         return os.getpid()
     except OSError as err:
         msg = Error(err).indent(2)
-        raise Error(f"failed to get own PID:\n{msg}") from None
+        raise Error(f"Failed to get own PID:\n{msg}") from None
 
-def get_pgid(pid):
+def get_pgid(pid: int) -> int:
     """
-    Return process group ID of a process with PID 'pid'. The arguments are as follows.
-      * pid - process ID to return the process group ID for.
+    Return the group ID of a process with the given PID.
+
+    Args:
+        pid: The process ID to return the process group ID for.
+
+    Returns:
+        int: The group ID of the specified process.
     """
 
     try:
         return os.getpgid(pid)
     except OSError as err:
         msg = Error(err).indent(2)
-        raise Error(f"failed to get group ID of process with PID {pid}:\n{msg}") from None
+        raise Error(f"Failed to get process group ID for PID {pid}:\n{msg}") from None
 
-def get_username(uid=None):
+def get_username(uid: int | None = None) -> str:
     """
-    Return username of current process of by UID. The arguments are as follows.
-      * uid - an optional integer user ID value to return the user name for (default is UID of the
-              current process).
+    Return the username of the current process or by UID.
+
+    Args:
+        uid: The user ID to return the username for. Defaults to the UID of the current process.
+
+    Returns:
+        str: The username of the specified UID.
     """
 
     try:
@@ -58,73 +76,84 @@ def get_username(uid=None):
             uid = os.getuid()
     except OSError as err:
         msg = Error(err).indent(2)
-        raise Error(f"failed to detect user name of current process:\n{msg}") from None
+        raise Error(f"Failed to detect username of current process:\n{msg}") from None
 
     try:
         return pwd.getpwuid(uid).pw_name
     except KeyError as err:
         msg = Error(err).indent(2)
-        raise Error(f"failed to get user name for UID {uid}:\n{msg}") from None
+        raise Error(f"Failed to get username for UID {uid}:\n{msg}") from None
 
-def str_to_int(snum, base=0, what=None):
+def str_to_int(snum: str | int, base: int = 0, what: str | None = None) -> int:
     """
-    Convert a string to an integer numeric value. The arguments are as follows.
-      * snum - the value to convert to 'int'. Should be a string or an integer.
-      * base - base of 'snum' (default - auto-detect based on the prefix).
-      * what - a string describing the value to convert, for the possible error message.
+    Convert a string to an integer numeric value.
+
+    Args:
+        snum: The value to convert to 'int'.
+        base: Base of 'snum'. Defaults to auto-detect based on the prefix.
+        what: A string describing the value to convert, for the possible error message.
+
+    Returns:
+        int: The converted integer value.
     """
 
     try:
         num = int(str(snum), base)
     except (ValueError, TypeError):
-        if not what is None:
+        if what is None:
             what = "value"
-        # Make sure the 'base' was a sane number.
         try:
             str_to_int(base)
         except (ValueError, TypeError) as err:
             msg = Error(str(err)).indent(2)
-            raise Error(f"BUG: bad base value {base} when converting bad {what} '{snum}':\n"
+            raise Error(f"BUG: Bad base value {base} when converting bad {what} '{snum}':\n"
                         f"{msg}") from err
 
         if base != 0 and base < 2:
-            raise Error(f"BUG: bad base value {base} when converting bad {what} '{snum}': "
-                        f"must be greater than 2 or 0") from None
+            raise Error(f"BUG: Bad base value {base} when converting bad {what} '{snum}': must be "
+                        f"greater than 2 or 0") from None
 
         if base:
-            msg = "a base {base} integer"
+            msg = f"a base {base} integer"
         else:
             msg = "an integer"
-        raise Error(f"bad {what} '{snum}': should be {msg}") from None
+        raise Error(f"Bad {what} '{snum}': should be {msg}") from None
 
     return num
 
-def str_to_num(snum, what=None):
+def str_to_num(snum: str | int, what: str | None = None) -> int | float:
     """
-    Convert a string to a numeric value, either 'int' or 'float'. The arguments are as follows.
-      * snum - the value to convert to 'int' or 'float'. Should be a string or a numeric value.
-      * what - a string describing the value to convert, for the possible error message.
+    Convert a string to a numeric value, either 'int' or 'float'.
+
+    Args:
+        snum: The value to convert to 'int' or 'float'.
+        what: A string describing the value to convert, for the possible error message.
+
+    Returns:
+        int or float: The converted numeric value.
     """
 
     try:
-        num = int(str(snum), 0)
+        return int(str(snum), 0)
     except (ValueError, TypeError):
         try:
-            num = float(str(snum))
+            return float(str(snum))
         except (ValueError, TypeError):
-            if not what is None:
+            if what is None:
                 what = "value"
-            pfx = f"bad {what} '{snum}'"
-            raise Error(f"{pfx}: should be an integer of floating point number") from None
+            pfx = f"Bad {what} '{snum}'"
+            raise Error(f"{pfx}: should be an integer or floating point number") from None
 
-    return num
-
-def is_int(value, base=0):
+def is_int(value: str | int, base: int = 0) -> bool:
     """
-    Return 'True' if 'value' can be converted to an integer using 'int()' and 'False' otherwise. The
-    arguments are as follows.
-      * value - the value to convert to an integer.
-      * base - base of the value (auto-detect by default based on the prefix).
+    Check if 'value' can be converted to 'int' type'.
+
+    Args:
+        value: The value to check.
+        base: Base of the value. Defaults to auto-detect based on the prefix.
+
+    Returns:
+        bool: True if 'value' can be converted to 'int' type, False otherwise.
     """
 
     try:
@@ -136,9 +165,15 @@ def is_int(value, base=0):
             return False
     return True
 
-def is_float(value):
+def is_float(value: str | float) -> bool:
     """
-    Return 'True' if 'value' can be converted to a float using 'float()' and 'False' otherwise.
+    Check if 'value' can be converted to 'float' type'.
+
+    Args:
+        value: The value to check.
+
+    Returns:
+        bool: True if 'value' can be converted to 'float' type, False otherwise.
     """
 
     try:
@@ -147,10 +182,15 @@ def is_float(value):
         return False
     return True
 
-def is_num(value):
+def is_num(value: str | int | float) -> bool:
     """
-    Return 'True' if 'value' can be converted to an integer using 'int()' or to a float using
-    'float()'. Returns 'False' otherwise.
+    Check if 'value' can be converted to 'int' or 'float' types.
+
+    Args:
+        value: The value to check.
+
+    Returns:
+        bool: True if 'value' can be converted to 'int' or 'float' types, False otherwise.
     """
 
     try:
@@ -163,72 +203,99 @@ def is_num(value):
 
     return True
 
-def validate_value_in_range(value, minval, maxval, what=None):
+def validate_value_in_range(value: int | float, minval: int | float,
+                            maxval: int | float, what: str | None = None):
     """
-    Validate that value 'value' is in the ['minval', 'maxval'] range. The arguments are as follows.
-      * value - the value to validate.
-      * minval - the minimum allowed value for 'value'.
-      * maxval - the maximum allowed value for 'value'.
-      * what - a string describing the value that is being validated, for the possible error
-               message.
+    Validate that 'value' is in the ['minval', 'maxval'] range.
+
+    Args:
+        value: The value to validate.
+        minval: The minimum allowed value for 'value'.
+        maxval: The maximum allowed value for 'value'.
+        what: A string describing the value that is being validated, for the possible error message.
     """
 
     if value < minval or value > maxval:
-        if not what is None:
+        if what is None:
             what = "value"
         raise Error(f"{what} '{value}' is out of range, should be within [{minval},{maxval}]")
 
-def validate_range(minval, maxval, min_limit=None, max_limit=None, what=None):
+def validate_range(minval: int | float, maxval: int | float, min_limit: int | float | None = None,
+                   max_limit: int | float | None = None, what: str | None = None):
     """
-    Validate correctness of range ['minval', 'maxval']. The arguments are as follows.
-      * minval - the minimum value (first number in the range).
-      * maxval - the maximum value (second number in the range).
-      * min_limit - the minimum allowed value for 'minval'.
-      * max_limit - the maximum allowed value for 'maxval'.
-      * what - a string describing the range that is being validated, for the possible error
-               messages.
+    Validate correctness of range ['minval', 'maxval'].
+
+    Args:
+        minval: The minimum value (first number in the range).
+        maxval: The maximum value (second number in the range).
+        min_limit: The minimum allowed value for 'minval'.
+        max_limit: The maximum allowed value for 'maxval'.
+        what: A string describing the range that is being validated, for the possible error
+              messages.
     """
 
-    if not what is None:
+    if what is None:
         what = "range"
-    pfx = f"bad {what} '[{minval},{maxval}]'"
+    pfx = f"Bad {what} '[{minval},{maxval}]'"
 
     if minval > maxval:
-        raise Error(f"{pfx}: min. value '{minval}' should not be greater than max. value "
+        raise Error(f"{pfx}: Min. value '{minval}' should not be greater than max. value "
                     f"'{maxval}'")
 
     if min_limit is not None:
         if max_limit is not None:
             assert max_limit >= min_limit
         if minval < min_limit:
-            raise Error(f"{pfx}: should be within '[{min_limit},{max_limit}]'")
+            raise Error(f"{pfx}: Should be within '[{min_limit},{max_limit}]'")
 
     if max_limit is not None:
         if maxval > max_limit:
-            raise Error(f"{pfx}: should be within '[{min_limit},{max_limit}]'")
+            raise Error(f"{pfx}: Should be within '[{min_limit},{max_limit}]'")
 
-def is_iterable(value):
-    """Return 'True' if 'value' is iterable collection (not string) and 'False' otherwise."""
+def is_iterable(value: str | list | tuple | set | dict) -> bool:
+    """
+    Check if 'value' is an iterable collection. The 'str' type is not considered to be an iterable
+    collection.
+
+    Args:
+        value: The value to check.
+
+    Returns:
+        bool: True if 'value' is an iterable collection, False otherwise.
+    """
+
     try:
         iter(value)
     except TypeError:
         return False
     return not isinstance(value, str)
 
-def list_dedup(elts):
-    """Return list of unique elements in 'elts'."""
+def list_dedup(elts: list) -> list:
+    """
+    Return a list of unique elements in 'elts'.
+
+    Args:
+        elts: The list of elements.
+
+    Returns:
+        list: A list of unique elements.
+    """
 
     return list(dict.fromkeys(elts))
 
-def split_csv_line(csv_line, sep=",", dedup=False, keep_empty=False):
+def split_csv_line(csv_line: str, sep: str = ",", dedup: bool = False,
+                   keep_empty: bool = False) -> list[str]:
     """
-    Split a comma-separated values line (a string of values separated by a comma) and return the
-    list of the values. The arguments are as follows.
-      * csv_line - the comma-separated line to split.
-      * sep - the separator character (comma by default).
-      * dedup - if 'True', remove duplicated elements from the returned list.
-      * keep_empty - if 'True', keep empty values. E.g. split_csv_line(",cpu0", keep_empty=True)
-                     would return ["", "cpu0"].
+    Split a comma-separated values line and return the list of values.
+
+    Args:
+        csv_line: The comma-separated line to split.
+        sep: The separator character. Defaults to comma.
+        dedup: If True, remove duplicated elements from the returned list.
+        keep_empty: If True, keep empty values.
+
+    Returns:
+        list: A list of values.
     """
 
     result = []
@@ -244,24 +311,29 @@ def split_csv_line(csv_line, sep=",", dedup=False, keep_empty=False):
         return list_dedup(result)
     return result
 
-def split_csv_line_int(csv_line, sep=",", dedup=False, base=0, what=None):
+def split_csv_line_int(csv_line: str, sep: str = ",", dedup: bool = False, base: int = 0,
+                       what: str | None = None) -> list[int]:
     """
-    Split a comma-separated integer values line (a string with integer values of value rages
-    separated by a comma) and return the list of the integer values. The arguments are as follows.
-      * csv_line - the comma-separated line to split.
-      * sep - the separator character (comma by default).
-      * dedup - if 'True', remove duplicated elements from the returned list.
-      * base - base of the values in 'csv_line' (default - auto-detect based on the prefix).
-      * what - a string describing the values in 'csv_line', for the possible error message.
+    Split a comma-separated values line consisting of integers return the list of integer values.
 
-    Here is an example.
-      - Input: csv_line = "0,1-3,7"
-      - Output: [0, 1, 2, 3, 7].
+    Args:
+        csv_line: The comma-separated line to split.
+        sep: The separator character. Defaults to comma.
+        dedup: If True, remove duplicated elements from the returned list.
+        base: Base of the values in 'csv_line'. Defaults to auto-detect based on the prefix.
+        what: A string describing the values in 'csv_line', for the possible error message.
+
+    Returns:
+        list: A list of integer values.
+
+    Example:
+        Input: csv_line = "0,1-3,7"
+        Output: [0, 1, 2, 3, 7].
     """
 
     vals = split_csv_line(csv_line=csv_line, sep=sep, dedup=dedup)
     if what is None:
-        what = "value'"
+        what = "value"
 
     result = []
     for val in vals:
@@ -269,44 +341,45 @@ def split_csv_line_int(csv_line, sep=",", dedup=False, base=0, what=None):
             result.append(str_to_int(val, base=base, what=what))
             continue
 
-        # Handle a rage.
         range_vals = [range_val for range_val in val.split("-") if range_val]
         if len(range_vals) != 2:
-            raise Error(f"bad {what} '{csv_line}: error in '{val}': should be two integers "
+            raise Error(f"Bad {what} '{csv_line}': error in '{val}': should be two integers "
                         f"separated by '-'")
 
-        range_vals = [str_to_int(range_val, base=base, what=what) for range_val in range_vals]
-        if range_vals[0] > range_vals[1]:
-            raise Error(f"bad {what} '{csv_line}': error in range '{val}': the first number should "
+        rvals = [str_to_int(rval, base=base, what=what) for rval in range_vals]
+        if rvals[0] > rvals[1]:
+            raise Error(f"Bad {what} '{csv_line}': error in range '{val}': the first number should "
                         f"be smaller than the second")
 
-        result += range(range_vals[0], range_vals[1] + 1)
+        result += range(rvals[0], rvals[1] + 1)
 
     return result
 
-def parse_int_list(nums, sep=",", dedup=False, base=0, what=None):
+def parse_int_list(nums: str | int | list[str | int] | tuple[str | int], sep: str = ",",
+                   dedup: bool = False, base: int = 0, what: str | None = None) -> list[int]:
     """
-    Same as 'split_csv_line_int()', but also accepts non-strings on input. The arguments are as
-    follows.
-      * nums - the numbers to parse.
-      * sep - the possible separator character (comma by default).
-      * dedup - if 'True', remove duplicated elements from the returned list.
-      * base - base of the values in 'nums' (default - auto-detect based on the prefix).
-      * what - a string describing the values in 'nums', for the possible error message.
+    Same as 'split_csv_line_int()', but also accepts non-strings on input.
 
-    Here are some examples.
-      - Input: nums = "0,1-3,7"
-      - Output: [0, 1, 2, 3, 7].
-      - Input: nums = ["1", "4-7]
-      - Output: [1, 4, 5, 6, 7].
+    Args:
+        nums: The numbers to parse.
+        sep: The possible separator character. Defaults to comma.
+        dedup: If True, remove duplicated elements from the returned list.
+        base: Base of the values in 'nums'. Defaults to auto-detect based on the prefix.
+        what: A string describing the values in 'nums', for the possible error message.
+
+    Returns:
+        list: A list of integer values.
+
+    Examples:
+        Input: nums = "0,1-3,7"
+        Output: [0, 1, 2, 3, 7].
+        Input: nums = ["1", "4-7"]
+        Output: [1, 4, 5, 6, 7].
     """
 
-    if isinstance(nums, int):
-        nums = [nums]
-    elif not is_iterable(nums):
+    if isinstance(nums, (int, str)):
         nums = [nums]
 
-    if not isinstance(nums, str):
-        nums = sep.join([str(num) for num in nums])
+    nums = sep.join([str(num) for num in nums])
 
     return split_csv_line_int(nums, sep=sep, dedup=dedup, base=base, what=what)
