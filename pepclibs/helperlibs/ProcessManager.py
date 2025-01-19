2c137efbaa20dfed1f497a7561904751141b5f93
Artem Bityutskiy <artem.bityutskiy@linux.intel.com>
2025-01-19 09:36:09 +0200

    ProcessManager: update (C) date
    Signed-off-by: Artem Bityutskiy <artem.bityutskiy@linux.intel.com>

--- pepclibs/helperlibs/ProcessManager.py
+++ pepclibs/helperlibs/ProcessManager.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 # vim: ts=4 sw=4 tw=100 et ai si
 #
-# Copyright (C) 2020-2021 Intel Corporation
+# Copyright (C) 2020-2025 Intel Corporation
 # SPDX-License-Identifier: BSD-3-Clause
 #
 # Author: Artem Bityutskiy <artem.bityutskiy@linux.intel.com>
