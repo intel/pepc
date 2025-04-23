a4879bebcab5813b26ec51aa7b95ba490e4147a2
Artem Bityutskiy <artem.bityutskiy@linux.intel.com>
2025-04-23 19:30:36 +0300

    ProcessManager: annotate and modernize the module
    Signed-off-by: Artem Bityutskiy <artem.bityutskiy@linux.intel.com>

--- pepclibs/helperlibs/ProcessManager.py
+++ pepclibs/helperlibs/ProcessManager.py
@@ -16,7 +16,7 @@ as if it was a local host.
 """
 
 import contextlib
-from typing import Union
+from typing import Union, Any, cast
 from pepclibs.helperlibs.Exceptions import Error
 from pepclibs.helperlibs import LocalProcessManager, SSHProcessManager, EmulProcessManager
 # pylint: disable-next=unused-import
@@ -28,61 +28,72 @@ ProcessManagerType = Union[LocalProcessManager.LocalProcessManager,
 
 ProcessType = Union[LocalProcessManager.LocalProcess, SSHProcessManager.SSHProcess]
 
-def _check_for_none(hostname, **kwargs):
+def _check_for_none(hostname: str, kwargs: dict[str, Any]):
     """
-    A helper function that makes sure that argument 'kwargs' are all set to 'None'.
+    Ensure all keyword arguments in 'kwargs' are set to 'None'.
+
+    Args:
+        hostname: The hostname associated with the check, used for error messages.
+        kwargs: Arbitrary keyword arguments to validate.
+
+    Raises:
+        Error: If any value in 'kwargs' is not 'None'.
     """
 
     for name, val in kwargs.items():
         if val is not None:
-            raise Error(f"BUG: get_pman: hostname is '{hostname}', but argument '{name}' is not "
+            raise Error(f"BUG: get_pman(): hostname is '{hostname}', but argument '{name}' is not "
                         f"'None'. Instead, it is '{val}'")
 
-def get_pman(hostname, username=None, privkeypath=None, timeout=None) -> ProcessManagerType:
+def get_pman(hostname: str,
+             username: str | None = None,
+             privkeypath: str | None =None,
+             timeout: int | float | None = None) -> ProcessManagerType:
     """
-    Creates and returns a process manager object for host 'hostname'. At the moment there are
-    basically 3 possibilities.
-     1. 'hostname' is "localhost".
-        1.1. If 'username' is 'None', then create and return a 'LocalProcessManager' object, which
-             provides an efficient way of managing local processes.
-        1.2 If 'username' is not 'None', then create and return an 'SSHProcessManager' object,
-            which will connect to local host over SSH and manage processes over a local SSH
-            connection. This is a less efficient way of managing processes, but may be useful for,
-            say, debugging purposes.
-     2. 'hostname' starts with "emulation". Create and return an 'EmulProcessManager' object. This
-        object is used for testing purposes only.
-     3. 'hostname' is not "localhost" and does not start with "emulation". Create and return an
-        'SSHProcessManager' object, which will be connected to the 'hostname' host over SSH and
-        will manage processes on the 'hostname' host over SSH.
+    Create and return a process manager object for the specified host.
 
+    Determine the appropriate process manager to use based on the `hostname` argument and return an
+    instance of the corresponding process manager class. The following cases are handled:
 
-    The arguments are as follows.
-      * hostname - the host name to create a process manager object for.
-      * username - the user name to use for logging into the 'hostname' host over SSH.
-      * privkeypath - path to SSH private key to use for logging into the host.
-      * timeout - the SSH connection time out in seconds.
+    1. If 'hostname' is "localhost":
+        - If 'username' is None, return a 'LocalProcessManager' object for efficient
+          management of local processes.
+        - If 'username' is not None, return an 'SSHProcessManager' object that connects
+          to the local host over SSH. This is less efficient but useful for debugging.
+    2. If 'hostname' starts with "emulation":
+        - Return an 'EmulProcessManager' object, which is used for testing purposes.
+    3. For all other cases:
+        - Return an 'SSHProcessManager' object that connects to the specified host
+          over SSH and manages processes on the remote host.
 
-    The 'hostname' argument is used for all types of process managers. The 'username',
-    'privkeypath', and 'timeout' arguments are used only for 'SSHProcessManager'. Have to be 'None'
-    for everything else.
+    Args:
+         hostname: The host name to create a process manager object for.
+         username: The user name for logging into the host over SSH. Only used for
+                   'SSHProcessManager'.
+         privkeypath: Path to the SSH private key for authentication. Only used for
+                     'SSHProcessManager'.
+         timeout: The SSH connection timeout in seconds. Only used for
+                  'SSHProcessManager'.
 
-    Notes.
-    1. The preferred way of using this method is with the 'with' statement:
-          with get_pman() as pman:
-              do_something(pman)
-       The context manager then closes the object.
-    2. The alternative way is to close the object after it had been used.
-          pman = get_pman()
-          try:
-              do_something()
-          finally:
-              pman.close()
+    Returns:
+         An instance of the appropriate process manager class.
+
+    Usage examples:
+        1.  with get_pman(hostname) as pman:
+                pman.run_verify(command)
+
+        2.  pman = get_pman(hostname)
+            try:
+                pman.run_verify(command)
+            finally:
+                pman.close()
     """
 
     pman: ProcessManagerType | None = None
 
     if hostname == "localhost" and username is None:
-        _check_for_none(hostname, username=username, privkeypath=privkeypath, timeout=timeout)
+        kwargs = {"username": username, "privkeypath": privkeypath, "timeout": timeout}
+        _check_for_none(hostname, kwargs)
         pman = LocalProcessManager.LocalProcessManager()
     elif hostname.startswith("emulation"):
         pman = EmulProcessManager.EmulProcessManager(hostname=hostname)
@@ -92,28 +103,33 @@ def get_pman(hostname, username=None, privkeypath=None, timeout=None) -> Process
 
     return pman
 
-def pman_or_local(pman):
+def pman_or_local(pman: ProcessManagerType) -> ProcessManagerType:
     """
-    Return 'pman' if it is not 'None', otherwise return a new instance of 'LocalProcessManager'.
+    Return the provided process manager or a new 'LocalProcessManager' instance.
 
-    This helper is designed for situations when you have a function, which takes a 'pman' object on
-    input. However, your function allows for 'pman == None', in which case it falls back to a
-    'LocalProcessManager' object.
+    Problem:
+        A method accepts a 'pman=None' argument (a process manager object, with default value
+        'None'). By default, the 'LocalProcessManager' should be used. And 'pman' is used with
+        'with' statement, so it should be closed automatically, but only if it was created
+        internally, not when the caller passed it.
 
-    Here is how this helper is supposed to be used:
+    Solution:
+        The 'pman_or_local()' function takes the process manager object as an argument. If the
+        argument is 'None', it creates a new 'LocalProcessManager' instance and returns it.
+        Otherwise, it returns the provided process manager object, but wrapped in a context manager
+        that does not call the '__exit__()' method. This way, the caller is responsible for
+        managing the lifecycle of the provided process manager.
 
-      with pman_or_local(pman) as wpman:
-          do_stuff()
+    Args:
+        pman: The process manager object to use. If 'None', a new 'LocalProcessManager' instance
+              will be created.
 
-    If user provided a process manager object ('pman != None'), 'wpman' will be 'pman'. In this
-    case, the 'pman.__exit__()' will not be called, which is the right thing to do because 'pman'
-    came from a user, who is responsible for closing the process manager.
-
-    If user did not provide a process manager ('pman == None'), then a 'LocalProcessManager()'
-    instance will be created and closed upon exiting the 'with' context.
+    Returns:
+        ProcessManagerType: The provided process manager object if it is not 'None', otherwise a
+        new instance of 'LocalProcessManager', wrapped into a "nullcontext" context manager.
     """
 
     if pman:
-        return contextlib.nullcontext(enter_result=pman)
+        return cast(ProcessManagerType, contextlib.nullcontext(enter_result=pman))
 
     return LocalProcessManager.LocalProcessManager()
