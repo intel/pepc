1b6ed2c3dbd7f93b004a197d53bc3d33c9db26f9
Artem Bityutskiy <artem.bityutskiy@linux.intel.com>
2025-04-28 10:15:43 +0300

    EmulProcessManager: address mypy warning
    line 442: Argument 2 of "mkdtemp" is incompatible with supertype "LocalProcessManager"; supertype defines the argument type as "str | Path | None"
    
    Signed-off-by: Artem Bityutskiy <artem.bityutskiy@linux.intel.com>

--- pepclibs/helperlibs/EmulProcessManager.py
+++ pepclibs/helperlibs/EmulProcessManager.py
@@ -439,7 +439,7 @@ class EmulProcessManager(LocalProcessManager.LocalProcessManager):
         path = Path(self._get_basepath() / str(path).lstrip("/"))
         return super().is_socket(path)
 
-    def mkdtemp(self, prefix: str | None  = None, basedir: Path | None = None) -> Path:
+    def mkdtemp(self, prefix: str | None  = None, basedir: str | Path | None = None) -> Path:
         """
         Create a temporary directory and return its path. The arguments are as follows.
           * prefix - specifies the temporary directory name prefix.
