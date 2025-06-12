290e1c883c7df0a8511db980d0e28933913d4c08
Artem Bityutskiy <artem.bityutskiy@linux.intel.com>
2025-06-12 09:03:35 +0300

    MSR: split the module
    Split the module, provide _SimpleMSR module which does not depend on CPUInfo,
    so that it could be used from CPUInfo.
    
    Split just enough to make _SimpleMSR useful for current CPUInfo needs. More
    split could be done later.
    
    Signed-off-by: Artem Bityutskiy <artem.bityutskiy@linux.intel.com>

--- pepclibs/msr/MSR.py
+++ pepclibs/msr/MSR.py
@@ -8,7 +8,8 @@
 #          Antti Laakso <antti.laakso@linux.intel.com>
 
 """
-Provide a capability to read and write CPU Model Specific Registers.
+Provide a capability to read and write CPU Model Specific Registers, including write-through caching
+and transactions support.
 """
 
 from __future__ import annotations # Remove when switching to Python 3.10+.
@@ -16,11 +17,13 @@ from __future__ import annotations # Remove when switching to Python 3.10+.
 import pprint
 from typing import Generator, TypedDict, Literal, Sequence
 from pathlib import Path
-from pepclibs.helperlibs import Logging, LocalProcessManager, FSHelpers, KernelModule, Trivial
+from pepclibs.helperlibs import Logging
 from pepclibs.helperlibs import ClassHelpers
 from pepclibs.helperlibs.Exceptions import Error, ErrorVerifyFailed, ErrorNotFound
 from pepclibs.helperlibs.ProcessManager import ProcessManagerType
 from pepclibs._CPUInfoBaseTypes import ScopeNameType
+from pepclibs.msr import _SimpleMSR
+from pepclibs.msr._SimpleMSR import _CPU_BYTEORDER
 from pepclibs import _PerCPUCache, CPUInfo
 
 class _TransactionBufferItemTypedDict(TypedDict, total=False):
@@ -49,13 +52,12 @@ class _TransactionVerifyItemTypedDict(TypedDict, total=False):
     cpus: list[int]
     iosname: ScopeNameType
 
-_CPU_BYTEORDER: Literal["little", "big"] = "little"
-
 _LOG = Logging.getLogger(f"{Logging.MAIN_LOGGER_NAME}.pepc.{__name__}")
 
-class MSR(ClassHelpers.SimpleCloseContext):
+class MSR(_SimpleMSR.SimpleMSR):
     """
-    Provide a capability to read and write CPU Model Specific Registers.
+    Provide a capability to read and write CPU Model Specific Registers, including write-through
+    caching and transactions support.
 
     Public methods overview.
 
@@ -100,24 +102,13 @@ class MSR(ClassHelpers.SimpleCloseContext):
                           hardware directly.
         """
 
+        super().__init__(pman=pman)
+
         self._cpuinfo = cpuinfo
         self._enable_cache = enable_cache
 
-        self._close_pman = pman is None
-
-        if pman:
-            self._pman = pman
-        else:
-            self._pman = LocalProcessManager.LocalProcessManager()
-
-        # MSR size in bits and bytes.
-        self.regbits = 64
-        self.regbytes = self.regbits // 8
-
-        self._msr_drv: KernelModule.KernelModule | None = None
-
         # The write-through per-CPU MSR values cache.
-        self._cache = _PerCPUCache.PerCPUCache(self._cpuinfo, enable_cache=self._enable_cache)
+        self._cache = _PerCPUCache.PerCPUCache(cpuinfo, enable_cache=self._enable_cache)
         # The transaction buffer. This is a dictionary of dictionaries, where the first key is the
         # CPU number, and the second key is the MSR address. The value is a dictionary with
         # transaction value and additional information.
@@ -125,59 +116,14 @@ class MSR(ClassHelpers.SimpleCloseContext):
         # Whether there is an ongoing transaction.
         self._in_transaction = False
 
-        self._ensure_dev_msr()
-
     def close(self):
         """Uninitialize the class object."""
 
-        if self._msr_drv:
-            try:
-                self._msr_drv.unload()
-            except Error as err:
-                _LOG.warning("Failed to unload the previously loaded MSR driver: %s", err.indent(2))
-
-        close_attrs = ("_pman", "_msr_drv", "_cache")
+        close_attrs = ("_cache",)
         unref_attrs = ("_cpuinfo",)
         ClassHelpers.close(self, close_attrs=close_attrs, unref_attrs=unref_attrs)
 
-    def _ensure_dev_msr(self):
-        """
-        Ensure that device nodes for accessing Model-Specific Registers (MSRs) are available.
-        Attempt to load the 'msr' kernel driver if the required device node does not exist.
-        """
-
-        cpus = self._cpuinfo.get_cpus()
-        dev_path = Path(f"/dev/cpu/{cpus[0]}/msr")
-        if self._pman.exists(dev_path):
-            return
-
-        drvname = "msr"
-        msg = f"File '{dev_path}' is not available{self._pman.hostmsg}\nMake sure your kernel" \
-              f"has the '{drvname}' driver enabled (CONFIG_X86_MSR)."
-        try:
-            self._msr_drv = KernelModule.KernelModule(drvname, pman=self._pman)
-        except Error as err:
-            raise Error(f"{msg}\n{err.indent(2)}") from err
-
-        try:
-            loaded = self._msr_drv.is_loaded()
-        except Error as err:
-            self._msr_drv.close()
-            self._msr_drv = None
-            raise Error(f"{msg}\n{err.indent(2)}") from err
-
-        if loaded:
-            self._msr_drv.close()
-            self._msr_drv = None
-            raise Error(msg)
-
-        try:
-            self._msr_drv.load()
-            FSHelpers.wait_for_a_file(dev_path, timeout=1, pman=self._pman)
-        except Error as err:
-            self._msr_drv.close()
-            self._msr_drv = None
-            raise Error(f"{msg}\n{err.indent(2)}") from err
+        super().close()
 
     def _add_for_transation(self,
                             regaddr: int,
@@ -392,85 +338,9 @@ for cpu, cpus_info in transaction_buffer.items():
         else:
             _LOG.debug("MSR transaction has been committed, but it was empty")
 
-    def _normalize_bits(self, bits: tuple[int, int] | list[int]) -> tuple[int, int]:
-        """
-        Validate and normalize a bits range.
-
-        Args:
-            bits: A tuple or list of two integers (msb, lsb) specifying the bit range to extract;
-                  msb is the most significant bit and lsb is the least significant bit.
-
-        Returns:
-            A tuple of two integers representing the normalized bit range.
-        """
-
-        orig_bits = bits
-        try:
-            if not Trivial.is_int(orig_bits[0]) or not Trivial.is_int(orig_bits[1]):
-                raise Error(f"Bad bits range '{bits}', must be a list or tuple of 2 integers")
-
-            bits = (int(orig_bits[0]), int(orig_bits[1]))
-
-            if bits[0] < bits[1]:
-                raise Error(f"Bad bits range ({bits[0]}, {bits[1]}), the first number must be "
-                            f"greater or equal to the second number")
-
-            bits_cnt = (bits[0] - bits[1]) + 1
-            if bits_cnt > self.regbits:
-                raise Error(f"Too many bits in ({bits[0]}, {bits[1]}), MSRs only have "
-                            f"{self.regbits} bits")
-        except TypeError:
-            raise Error(f"Bad bits range '{bits}', must be a list or tuple of 2 integers") from None
-
-        return bits
-
-    def get_bits(self, regval: int, bits: tuple[int, int] | list[int]) -> int:
-        """
-        Extract a range of bits from an MSR value.
-
-        Args:
-            regval: The MSR value to extract bits from.
-            bits: A tuple or list of two integers (msb, lsb) specifying the bit range to extract;
-                  msb is the most significant bit and lsb is the least significant bit.
-
-        Returns:
-            The integer value represented by the specified bit range, right-aligned to bit 0.
-        """
-
-        bits = self._normalize_bits(bits)
-        bits_cnt = (bits[0] - bits[1]) + 1
-        mask = (1 << bits_cnt) - 1
-        return (regval >> bits[1]) & mask
-
-    def _read_cpu_nocache(self, regaddr: int, cpu: int) -> int:
-        """
-        Read an MSR at the specified address for a given CPU without using the cache.
-
-        Args:
-            regaddr: The address of the MSR to read.
-            cpu: CPU number to read the MSR from.
-
-        Returns:
-            The value of the MSR as an integer.
-        """
-
-        path = Path(f"/dev/cpu/{cpu}/msr")
-        try:
-            with self._pman.open(path, "rb") as fobj:
-                fobj.seek(regaddr)
-                regval = fobj.read(self.regbytes)
-        except Error as err:
-            raise Error(f"Failed to read MSR '{regaddr:#x}' from file '{path}'"
-                        f"{self._pman.hostmsg}:\n{err.indent(2)}") from err
-
-        regval = int.from_bytes(regval, byteorder=_CPU_BYTEORDER)
-        _LOG.debug("CPU%d: MSR 0x%x: read 0x%x%s", cpu, regaddr, regval, self._pman.hostmsg)
-
-        return regval
-
     def _read_remote_optimized(self,
                                regaddr: int,
-                               cpus: list[int],
+                               cpus: Sequence[int],
                                iosname: ScopeNameType) -> Generator[tuple[int, int], None, None]:
         """
         Optimized method for reading MSR values from a remote host.
@@ -489,6 +359,11 @@ for cpu, cpus_info in transaction_buffer.items():
             from the MSR.
         """
 
+        if not self._enable_cache:
+            yield from super()._cpus_read_remote(regaddr, cpus)
+            return
+
+
         # CPU numbers to read the MSR for (subset of 'cpus').
         do_read = []
         # CPU numbers the MSR should not be read for. Instead, the MSR values for these CPU numbres
@@ -500,64 +375,28 @@ for cpu, cpus_info in transaction_buffer.items():
             if cpu in dont_read:
                 continue
 
-            try:
-                self._cache.get(regaddr, cpu)
+            if self._cache.is_cached(regaddr, cpu):
+                dont_read.add(cpu)
                 continue
-            except ErrorNotFound:
-                if self._enable_cache:
-                    # The cache is enabled, but MSR value is not available from there. Read the MSR
-                    # only for 'iosname' sibling CPU, because MSR value should be the same for the
-                    # siblings.
-                    for sibling in self._cpuinfo.get_cpu_siblings(cpu, iosname):
-                        if sibling == cpu:
-                            do_read.append(sibling)
-                        else:
-                            dont_read.add(sibling)
+
+            # Read the MSR only for 'iosname' sibling CPU, because MSR value should be the same
+            # for the siblings.
+            for sibling in self._cpuinfo.get_cpu_siblings(cpu, iosname):
+                if sibling == cpu:
+                    do_read.append(sibling)
                 else:
-                    # The cache is disabled. Technically, it is not necessary to do anything
-                    # differently in this case. But emulate the 'read()' method behavior for
-                    # consistency: ignore the 'iosname' and just read the MSR for all CPUs in
-                    # 'cpus'.
-                    do_read.append(cpu)
+                    dont_read.add(sibling)
 
         if not do_read:
             for cpu in cpus:
                 yield cpu, self._cache.get(regaddr, cpu)
             return
 
-        python_path = self._pman.get_python_path()
-        cpus_str = ",".join([str(cpu) for cpu in do_read])
-        cmd = f"""{python_path} -c '
-cpus = [{cpus_str}]
-for cpu in cpus:
-    path = "/dev/cpu/%d/msr" % cpu
-    with open(path, "rb") as fobj:
-        fobj.seek({regaddr})
-        regval = fobj.read({self.regbytes})
-        regval = int.from_bytes(regval, byteorder="{_CPU_BYTEORDER}")
-        print("%d,%d" % (cpu, regval))
-'"""
-
-        stdout, _ = self._pman.run_verify(cmd, join=False)
-        regvals: dict[int, int] = {}
-
-        for line in stdout:
-            split = Trivial.split_csv_line(line.strip())
-            if len(split) != 2:
-                raise Error("BUG: bad MSR read script line '{line}'")
-
-            cpu = Trivial.str_to_int(split[0], what="CPU number")
-            regval = Trivial.str_to_int(split[1], what=f"MSR {regaddr:#x} value on CPU {cpu}")
-
-            regvals[cpu] = regval
+        for cpu, regval in super()._cpus_read_remote(regaddr, do_read):
             self._cache.add(regaddr, cpu, regval, sname=iosname)
 
         for cpu in cpus:
-            if cpu not in regvals:
-                regval = self._cache.get(regaddr, cpu)
-            else:
-                regval = regvals[cpu]
-
+            regval = self._cache.get(regaddr, cpu)
             yield cpu, regval
 
     def _read(self,
@@ -591,7 +430,7 @@ for cpu in cpus:
                 regval = self._cache.get(regaddr, cpu)
             except ErrorNotFound:
                 # Not in the cache, read from the HW.
-                regval = self._read_cpu_nocache(regaddr, cpu)
+                regval = super().cpu_read(regaddr, cpu)
                 self._cache.add(regaddr, cpu, regval, sname=iosname)
 
             yield cpu, regval
@@ -686,64 +525,6 @@ for cpu in cpus:
         regval = self.read_cpu(regaddr, cpu, iosname=iosname)
         return self.get_bits(regval, bits)
 
-    def set_bits(self, regval: int, bits: tuple[int, int] | list[int], val: int) -> int:
-        """
-        Set a range of bits in an MSR value to a specified value and return the modified result.
-
-        Args:
-            regval: The MSR value in which to set the bits.
-            bits: A tuple or list of two integers (msb, lsb) specifying the bit range to set;
-                  msb is the most significant bit and lsb is the least significant bit.
-            val: The value to set in the bits range to.
-        """
-
-        bits = self._normalize_bits(bits)
-        bits_cnt = (bits[0] - bits[1]) + 1
-        max_val = (1 << bits_cnt) - 1
-
-        if not Trivial.is_int(val):
-            raise Error(f"Bad value {val}, please provide a positive integer")
-        val = int(val)
-
-        if val > max_val:
-            raise Error(f"Too large value {val} for bits range ({bits[0]}, {bits[1]})")
-
-        clear_mask = max_val << bits[1]
-        set_mask = val << bits[1]
-        return (regval & ~clear_mask) | set_mask
-
-    def _write_cpu_nocache(self,
-                           regaddr: int,
-                           regval: int,
-                           cpu: int,
-                           regval_bytes: bytes | None = None):
-        """
-        Write a value to an MSR for a specific CPU without using cache.
-
-        Args:
-            regaddr: The address of the MSR to write to.
-            regval: The value to write to the MSR.
-            cpu: CPU number to write the MSR on.
-            regval_bytes: The value to write as a bytes object. If not provided, regval is converted
-                          to bytes. If provided, regval is ignored and regval_bytes is written to
-                          the MSR. In other words, this is an optimization saving the "to_bytes()"
-                          conversion step.
-        """
-
-        if regval_bytes is None:
-            regval_bytes = regval.to_bytes(self.regbytes, byteorder=_CPU_BYTEORDER)
-
-        path = Path(f"/dev/cpu/{cpu}/msr")
-        with self._pman.open(path, "r+b") as fobj:
-            try:
-                fobj.seek(regaddr)
-                fobj.write(regval_bytes)
-                fobj.flush()
-                _LOG.debug("CPU%d: MSR 0x%x: wrote 0x%x", cpu, regaddr, regval)
-            except Error as err:
-                raise Error(f"Failed to write '{regval:#x}' to MSR '{regaddr:#x}' of CPU "
-                            f"{cpu}{self._pman.hostmsg} (file '{path}'):\n{err.indent(2)}") from err
-
     def write(self,
               regaddr: int,
               regval: int,
@@ -784,7 +565,7 @@ for cpu in cpus:
             if not self._in_transaction:
                 if regval_bytes is None:
                     regval_bytes = regval.to_bytes(self.regbytes, byteorder=_CPU_BYTEORDER)
-                self._write_cpu_nocache(regaddr, regval, cpu, regval_bytes=regval_bytes)
+                super().cpu_write(regaddr, regval, cpu, regval_bytes=regval_bytes)
             else:
                 self._add_for_transation(regaddr, regval, cpu, verify, iosname)
 
---
+++ pepclibs/msr/_SimpleMSR.py
@@ -0,0 +1,301 @@
+# -*- coding: utf-8 -*-
+# vim: ts=4 sw=4 tw=100 et ai si
+#
+# Copyright (C) 2020-2025 Intel Corporation
+# SPDX-License-Identifier: BSD-3-Clause
+#
+# Author: Artem Bityutskiy <artem.bityutskiy@linux.intel.com>
+
+"""
+Provide a capability to read and write CPU Model Specific Registers.
+"""
+
+from typing import Literal, Generator, Sequence
+from pathlib import Path
+from pepclibs.helperlibs import ClassHelpers, Trivial
+from pepclibs.helperlibs import Logging, LocalProcessManager, KernelModule, FSHelpers
+from pepclibs.helperlibs.ProcessManager import ProcessManagerType
+from pepclibs.helperlibs.Exceptions import Error
+
+_LOG = Logging.getLogger(f"{Logging.MAIN_LOGGER_NAME}.pepc.{__name__}")
+
+_CPU_BYTEORDER: Literal["little", "big"] = "little"
+
+class SimpleMSR(ClassHelpers.SimpleCloseContext):
+    """
+    Provide a capability to read and write CPU Model Specific Registers.
+    """
+
+    def __init__(self, pman: ProcessManagerType | None = None):
+        """
+        Initialize a class instance.
+
+        Args:
+            pman: The process manager object that defines the target host. If not provided, a local
+                  process manager will be used.
+        """
+
+        if pman:
+            self._pman = pman
+        else:
+            self._pman = LocalProcessManager.LocalProcessManager()
+
+        self._close_pman = pman is None
+
+        # MSR size in bits and bytes.
+        self.regbits = 64
+        self.regbytes = self.regbits // 8
+
+        self._msr_drv: KernelModule.KernelModule | None = None
+
+        self._ensure_dev_msr()
+
+    def close(self):
+        """Uninitialize the class object."""
+
+        if self._msr_drv:
+            try:
+                self._msr_drv.unload()
+            except Error as err:
+                _LOG.warning("Failed to unload the previously loaded MSR driver: %s", err.indent(2))
+
+        close_attrs = ("_pman", "_msr_drv")
+        ClassHelpers.close(self, close_attrs=close_attrs)
+
+    def _ensure_dev_msr(self):
+        """
+        Ensure that device nodes for accessing Model-Specific Registers (MSRs) are available.
+        Attempt to load the 'msr' kernel driver if the required device node does not exist.
+        """
+
+        dev_path = Path("/dev/cpu/0/msr")
+        if self._pman.exists(dev_path):
+            return
+
+        drvname = "msr"
+        msg = f"File '{dev_path}' is not available{self._pman.hostmsg}\nMake sure your kernel" \
+              f"has the '{drvname}' driver enabled (CONFIG_X86_MSR)."
+        try:
+            self._msr_drv = KernelModule.KernelModule(drvname, pman=self._pman)
+        except Error as err:
+            raise Error(f"{msg}\n{err.indent(2)}") from err
+
+        try:
+            loaded = self._msr_drv.is_loaded()
+        except Error as err:
+            self._msr_drv.close()
+            self._msr_drv = None
+            raise Error(f"{msg}\n{err.indent(2)}") from err
+
+        if loaded:
+            self._msr_drv.close()
+            self._msr_drv = None
+            raise Error(msg)
+
+        try:
+            self._msr_drv.load()
+            FSHelpers.wait_for_a_file(dev_path, timeout=1, pman=self._pman)
+        except Error as err:
+            self._msr_drv.close()
+            self._msr_drv = None
+            raise Error(f"{msg}\n{err.indent(2)}") from err
+
+    def _normalize_bits(self, bits: tuple[int, int] | list[int]) -> tuple[int, int]:
+        """
+        Validate and normalize a bits range.
+
+        Args:
+            bits: A tuple or list of two integers (msb, lsb) specifying the bit range to extract;
+                  msb is the most significant bit and lsb is the least significant bit.
+
+        Returns:
+            A tuple of two integers representing the normalized bit range.
+        """
+
+        orig_bits = bits
+        try:
+            if not Trivial.is_int(orig_bits[0]) or not Trivial.is_int(orig_bits[1]):
+                raise Error(f"Bad bits range '{bits}', must be a list or tuple of 2 integers")
+
+            bits = (int(orig_bits[0]), int(orig_bits[1]))
+
+            if bits[0] < bits[1]:
+                raise Error(f"Bad bits range ({bits[0]}, {bits[1]}), the first number must be "
+                            f"greater or equal to the second number")
+
+            bits_cnt = (bits[0] - bits[1]) + 1
+            if bits_cnt > self.regbits:
+                raise Error(f"Too many bits in ({bits[0]}, {bits[1]}), MSRs only have "
+                            f"{self.regbits} bits")
+        except TypeError:
+            raise Error(f"Bad bits range '{bits}', must be a list or tuple of 2 integers") from None
+
+        return bits
+
+    def get_bits(self, regval: int, bits: tuple[int, int] | list[int]) -> int:
+        """
+        Extract a range of bits from an MSR value.
+
+        Args:
+            regval: The MSR value to extract bits from.
+            bits: A tuple or list of two integers (msb, lsb) specifying the bit range to extract;
+                  msb is the most significant bit and lsb is the least significant bit.
+
+        Returns:
+            The integer value represented by the specified bit range, right-aligned to bit 0.
+        """
+
+        bits = self._normalize_bits(bits)
+        bits_cnt = (bits[0] - bits[1]) + 1
+        mask = (1 << bits_cnt) - 1
+        return (regval >> bits[1]) & mask
+
+    def _cpus_read_remote(self,
+                          regaddr: int,
+                          cpus: Sequence[int]) -> Generator[tuple[int, int], None, None]:
+        """
+        Optimized method for reading MSR values from a remote host.
+
+        Improve performance by generating and executing a small Python script on the remote host to
+        read the specified MSR for a set of CPUs. This approach is faster than opening and reading
+        multiple remote '/dev/msr/{cpu}' files from the local system.
+
+        Args:
+            regaddr: The address of the MSR to read.
+            cpus: CPU numbers to read the MSR from.
+            iosname: The name of the I/O scope, used to determine sibling CPUs.
+
+        Yields:
+            Tuples of (cpu, regval), where 'cpu' is the CPU number and 'regval' is the value read
+            from the MSR.
+        """
+
+        python_path = self._pman.get_python_path()
+        cpus_str = ",".join([str(cpu) for cpu in cpus])
+        cmd = f"""{python_path} -c '
+cpus = [{cpus_str}]
+for cpu in cpus:
+    path = "/dev/cpu/%d/msr" % cpu
+    with open(path, "rb") as fobj:
+        fobj.seek({regaddr})
+        regval = fobj.read({self.regbytes})
+        regval = int.from_bytes(regval, byteorder="{_CPU_BYTEORDER}")
+        print("%d,%d" % (cpu, regval))
+'"""
+
+        stdout, _ = self._pman.run_verify(cmd, join=False)
+
+        for line in stdout:
+            split = Trivial.split_csv_line(line.strip())
+            if len(split) != 2:
+                raise Error("BUG: bad MSR read script line '{line}'")
+
+            cpu = Trivial.str_to_int(split[0], what="CPU number")
+            regval = Trivial.str_to_int(split[1], what=f"MSR {regaddr:#x} value on CPU {cpu}")
+
+            yield cpu, regval
+
+    def cpus_read(self,
+                  regaddr: int,
+                  cpus: Sequence[int]) -> Generator[tuple[int, int], None, None]:
+        """
+        Read the specified MSR from specified CPUs and yield the result.
+
+        Args:
+            regaddr: Address of the MSR to read.
+            cpus: CPU numbers to read the MSR from. The numbers have to be validated and normalized
+                  by the caller.
+
+        Yields:
+            Tuple of (cpu, regval):
+                cpu: CPU number from which the MSR was read.
+                regval: Value read from the MSR.
+        """
+
+        if self._pman.is_remote:
+            yield from self._cpus_read_remote(regaddr, cpus)
+        else:
+            for cpu in cpus:
+                regval = self.cpu_read(regaddr, cpu)
+                yield cpu, regval
+
+    def cpu_read(self, regaddr: int, cpu: int) -> int:
+        """
+        Read an MSR at the specified address for a given CPU
+
+        Args:
+            regaddr: The address of the MSR to read.
+            cpu: CPU number to read the MSR from. The number has to be validated and normalized
+                 by the caller.
+
+        Returns:
+            The value of the MSR as an integer.
+        """
+
+        path = Path(f"/dev/cpu/{cpu}/msr")
+        try:
+            with self._pman.open(path, "rb") as fobj:
+                fobj.seek(regaddr)
+                regval = fobj.read(self.regbytes)
+        except Error as err:
+            raise Error(f"Failed to read MSR '{regaddr:#x}' from file '{path}'"
+                        f"{self._pman.hostmsg}:\n{err.indent(2)}") from err
+
+        regval = int.from_bytes(regval, byteorder=_CPU_BYTEORDER)
+        _LOG.debug("CPU%d: MSR 0x%x: read 0x%x%s", cpu, regaddr, regval, self._pman.hostmsg)
+
+        return regval
+
+    def set_bits(self, regval: int, bits: tuple[int, int] | list[int], val: int) -> int:
+        """
+        Set a range of bits in an MSR value to a specified value and return the modified result.
+
+        Args:
+            regval: The MSR value in which to set the bits.
+            bits: A tuple or list of two integers (msb, lsb) specifying the bit range to set;
+                  msb is the most significant bit and lsb is the least significant bit.
+            val: The value to set in the bits range to.
+        """
+
+        bits = self._normalize_bits(bits)
+        bits_cnt = (bits[0] - bits[1]) + 1
+        max_val = (1 << bits_cnt) - 1
+
+        if not Trivial.is_int(val):
+            raise Error(f"Bad value {val}, please provide a positive integer")
+        val = int(val)
+
+        if val > max_val:
+            raise Error(f"Too large value {val} for bits range ({bits[0]}, {bits[1]})")
+
+        clear_mask = max_val << bits[1]
+        set_mask = val << bits[1]
+        return (regval & ~clear_mask) | set_mask
+
+    def cpu_write(self, regaddr: int, regval: int, cpu: int, regval_bytes: bytes | None = None):
+        """
+        Write a value to an MSR for a specific CPU.
+
+        Args:
+            regaddr: The address of the MSR to write to.
+            regval: The value to write to the MSR.
+            cpu: CPU number to write the MSR on.
+            regval_bytes: The value to write as a bytes object. If not provided, regval is converted
+                          to bytes. If provided, regval is ignored and regval_bytes is written to
+                          the MSR. In other words, this is an optimization saving the "to_bytes()"
+                          conversion step.
+        """
+
+        if regval_bytes is None:
+            regval_bytes = regval.to_bytes(self.regbytes, byteorder=_CPU_BYTEORDER)
+
+        path = Path(f"/dev/cpu/{cpu}/msr")
+        with self._pman.open(path, "r+b") as fobj:
+            try:
+                fobj.seek(regaddr)
+                fobj.write(regval_bytes)
+                fobj.flush()
+                _LOG.debug("CPU%d: MSR 0x%x: wrote 0x%x", cpu, regaddr, regval)
+            except Error as err:
+                raise Error(f"Failed to write '{regval:#x}' to MSR '{regaddr:#x}' of CPU "
+                            f"{cpu}{self._pman.hostmsg} (file '{path}'):\n{err.indent(2)}") from err
