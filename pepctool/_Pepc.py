77f190bc84f4519617e0642c53c93b4963850633
Artem Bityutskiy <artem.bityutskiy@linux.intel.com>
2025-09-06 14:00:39 +0300

    _Pepc.py: address mypy warning
    Signed-off-by: Artem Bityutskiy <artem.bityutskiy@linux.intel.com>

--- pepctool/_Pepc.py
+++ pepctool/_Pepc.py
@@ -1108,7 +1108,7 @@ def main() -> int:
             return 0
 
         hostname: str = getattr(args, "hostname", "localhost")
-        username: str | None = getattr(args, "username", "")
+        username: str = getattr(args, "username", "")
         privkey: str | None = getattr(args, "privkey", None)
         timeout: int | float | None = getattr(args, "timeout", None)
         dataset: str | None = getattr(args, "dataset", None)
