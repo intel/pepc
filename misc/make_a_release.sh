295437efb5173b07fd72ab3811706b150ee3e422
Artem Bityutskiy <artem.bityutskiy@linux.intel.com>
2025-09-10 13:30:13 +0300

    misc/make_a_release.sh: Fix version vinding
    The regex for finding pepc version was broken after the _VERSION variable was
    nnotated. Fix it.
    
    Signed-off-by: Artem Bityutskiy <artem.bityutskiy@linux.intel.com>

--- misc/make_a_release.sh
+++ misc/make_a_release.sh
@@ -78,7 +78,7 @@ if [ $# -eq 1 ]; then
            fatal "please, provide new version in X.Y.Z format"
 elif [ $# -eq 0 ]; then
     # The new version was not provided, increment the current version umber.
-    sed_regex="^_VERSION = \"$VERSION_REGEX\"$"
+    sed_regex="^_VERSION[^=]*= \"$VERSION_REGEX\"$"
     ver_start="$(sed -n -e "s/$sed_regex/\1.\2./p" "$PEPC_FILE")"
     ver_end="$(sed -n -e "s/$sed_regex/\3/p" "$PEPC_FILE")"
     ver_end="$(($ver_end+1))"
