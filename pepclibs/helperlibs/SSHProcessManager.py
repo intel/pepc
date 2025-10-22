cd765a5a38079e7aa62a1039e81654053d44dbb5
Artem Bityutskiy <artem.bityutskiy@linux.intel.com>
2025-10-22 12:28:46 +0300

    SSHProcessManager: Avoid endless recursion
    Happens when the files to include do not exst. Fix it.
    
    Signed-off-by: Artem Bityutskiy <artem.bityutskiy@linux.intel.com>

--- pepclibs/helperlibs/SSHProcessManager.py
+++ pepclibs/helperlibs/SSHProcessManager.py
@@ -663,10 +663,11 @@ class SSHProcessManager(_ProcessManagerBase.ProcessManagerBase):
                     # The include directive may contain wildcards. Expand them. Sort the resulting
                     # list to have a deterministic order.
                     include_cfgfiles = sorted(glob.glob(cfg["include"]))
-                    optval = self._cfg_lookup(optname, hostname, username,
-                                              cfgfiles=include_cfgfiles)
-                    if optval:
-                        return optval
+                    if include_cfgfiles:
+                        optval = self._cfg_lookup(optname, hostname, username,
+                                                cfgfiles=include_cfgfiles)
+                        if optval:
+                            return optval
         finally:
             if old_username:
                 os.environ["USER"] = old_username
