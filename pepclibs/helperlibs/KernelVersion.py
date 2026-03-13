80a504309a67821188204958bf54a07b910bfea1
Artem Bityutskiy <artem.bityutskiy@linux.intel.com>
2026-03-13 16:09:35 +0000

    KernelVersion: Remove unused 'split' argument
    Signed-off-by: Artem Bityutskiy <artem.bityutskiy@linux.intel.com>

--- pepclibs/helperlibs/KernelVersion.py
+++ pepclibs/helperlibs/KernelVersion.py
@@ -85,23 +85,19 @@ def kver_ge(kver1, kver2):
 
     return not kver_lt(kver1, kver2)
 
-def get_kver(split=False, pman=None):
+def get_kver(pman=None):
     """
     Return version of the kernel running on the host associated with the 'pman' object (local host
     by default).
 
-    By default this function returns the kernel version string (e.g., "4.18.1-build0"), but if
-    'split' is 'True', this function returns the split kernel version (refer to 'split_kver()' for
-    the details).
+    By default this function returns the kernel version string (e.g., "4.18.1-build0").
     """
 
     with ProcessManager.pman_or_local(pman) as wpman:
         kver = wpman.run_verify_join("uname -r")[0].strip()
-        if split:
-            return split_kver(kver)
         return kver
 
-def get_kver_ktree(ktree, split=False, pman=None, makecmd=None):
+def get_kver_ktree(ktree, pman=None, makecmd=None):
     """
     Get version of the kernel in the kernel sources directory 'ktree'. The 'ktree' directory must
     contain an already configured kernel or it should be path to the kernel build directory if the
@@ -110,8 +106,6 @@ def get_kver_ktree(ktree, split=False, pman=None, makecmd=None):
     By default this function runs the 'make -C <ktree> --quiet -- kernelrelease' command to get the
     kernel version. However, you can use the 'makecmd' argument to override the 'make -C <ktree>'
     part of it.
-
-    The 'split' and 'pman' arguments are the same as in 'get_kver()'.
     """
 
     with ProcessManager.pman_or_local(pman) as wpman:
@@ -125,14 +119,11 @@ def get_kver_ktree(ktree, split=False, pman=None, makecmd=None):
             raise Error(f"cannot detect kernel version in '{ktree}':\n{err}\nMake sure kernel "
                         f"sources are configured.") from err
 
-    if split:
-        return split_kver(kver)
     return kver
 
-def get_kver_bin(path, split=False, pman=None):
+def get_kver_bin(path, pman=None):
     """
-    Get version of a kernel binary at 'path'. The 'split' and 'pman' arguments are the same as in
-    'get_kver()'.
+    Get version of a kernel binary at 'path'.
     """
 
     cmd = f"file -- {path}"
@@ -151,6 +142,4 @@ def get_kver_bin(path, split=False, pman=None):
         raise Error(f"{msg}\nFailed to find kernel version in the output.")
 
     kver = matchobj.group(1)
-    if split:
-        return split_kver(kver)
     return kver
