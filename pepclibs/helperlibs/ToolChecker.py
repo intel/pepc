b804fd1ff205aa96e9c4f7b55a8bd9f4a6b36bb6
Artem Bityutskiy <artem.bityutskiy@linux.intel.com>
2026-03-24 08:25:11 +0000

    ToolChecker: Modernize and Annotate
    Signed-off-by: Artem Bityutskiy <artem.bityutskiy@linux.intel.com>

--- pepclibs/helperlibs/ToolChecker.py
+++ pepclibs/helperlibs/ToolChecker.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 # vim: ts=4 sw=4 tw=100 et ai si
 #
-# Copyright (C) 2020-2022 Intel Corporation
+# Copyright (C) 2020-2026 Intel Corporation
 # SPDX-License-Identifier: BSD-3-Clause
 #
 # Authors: Antti Laakso <antti.laakso@intel.com>
@@ -12,73 +12,78 @@ Provide a capability of checking if a tool is installed on a Linux host, and gen
 suggestion if the OS package is not installed.
 """
 
+from __future__ import annotations # Remove when switching to Python 3.10+.
+
+import typing
 import contextlib
 from pathlib import Path
 from pepclibs.helperlibs import Logging, ClassHelpers
 from pepclibs.helperlibs.Exceptions import Error, ErrorNotFound
 
+if typing.TYPE_CHECKING:
+    from typing import Final
+    from pepclibs.helperlibs.ProcessManager import ProcessManagerType
+
 _LOG = Logging.getLogger(f"{Logging.MAIN_LOGGER_NAME}.pepc.{__name__}")
+
+# Common for CentOS, Fedora, RHEL, Debian, and Ubuntu.
+_COMMON_PKGINFO: Final[dict[str, str]] = {
+    "cat":       "coreutils",
+    "cc":        "gcc",
+    "clang":     "clang",
+    "find":      "findutils",
+    "depmod":    "kmod",
+    "dmesg":     "util-linux",
+    "gcc":       "gcc",
+    "libelf":    "elfutils-libelf-devel",
+    "lscpu":     "util-linux",
+    "lspci":     "pciutils",
+    "make":      "make",
+    "modprobe":  "kmod",
+    "phc2sys":   "linuxptp",
+    "rmmod":     "kmod",
+    "rsync":     "rsync",
+    "sync":      "coreutils",
+    "systemctl": "systemd",
+    "xargs":     "findutils",
+}
+
+# CentOS, Fedora, RHEL.
+_FEDORA_PKGINFO: Final[dict[str, str]] = {
+    **_COMMON_PKGINFO,
+    "tc":         "iproute-tc",
+    "bpftool":    "bpftool",
+    "libbpf":     "libbpf-devel",
+    "sch_etf.ko": "kernel-modules-extra",
+}
+
+# Ubuntu.
+_UBUNTU_PKGINFO: Final[dict[str, str]] = {
+    **_COMMON_PKGINFO,
+    "tc":         "iproute2",
+    "bpftool":    "linux-tools-common",
+    "libbpf":     "libbpf-dev",
+    "sch_etf.ko": "linux-modules",
+}
+
+# Debian.
+_DEBIAN_PKGINFO: Final[dict[str, str]] = {
+    **_COMMON_PKGINFO,
+    "tc":         "iproute2",
+    "bpftool":    "bpftool",
+    "libbpf":     "libbpf-dev",
+    "sch_etf.ko": "linux-modules",
+}
 
 #
 # The tools information dictionary. Map tool names to OS package names.
 #
-
-# Common for CentOS, Fedora, RHEL, Debian, and Ubuntu.
-_COMMON_PKGINFO = {
-    "cat"       : "coreutils",
-    "cc"        : "gcc",
-    "clang"     : "clang",
-    "find"      : "findutils",
-    "depmod"    : "kmod",
-    "dmesg"     : "util-linux",
-    "gcc"       : "gcc",
-    "libelf"    : "elfutils-libelf-devel",
-    "lscpu"     : "util-linux",
-    "lspci"     : "pciutils",
-    "make"      : "make",
-    "modprobe"  : "kmod",
-    "phc2sys"   : "linuxptp",
-    "rmmod"     : "kmod",
-    "rsync"     : "rsync",
-    "sync"      : "coreutils",
-    "systemctl" : "systemd",
-    "xargs"     : "findutils",
-}
-
-# CentOS, Fedora, RHEL.
-_FEDORA_PKGINFO = {
-    "tc"         : "iproute-tc",
-    "bpftool"    : "bpftool",
-    "libbpf"     : "libbpf-devel",
-    "sch_etf.ko" : "kernel-modules-extra",
-}
-
-# Ubuntu.
-_UBUNTU_PKGINFO = {
-    "tc"         : "iproute2",
-    "bpftool"    : "linux-tools-common",
-    "libbpf"     : "libbpf-dev",
-    "sch_etf.ko" : "linux-modules",
-}
-
-# Debian.
-_DEBIAN_PKGINFO = {
-    "tc"         : "iproute2",
-    "bpftool"    : "bpftool",
-    "libbpf"     : "libbpf-dev",
-    "sch_etf.ko" : "linux-modules",
-}
-
-_FEDORA_PKGINFO.update(_COMMON_PKGINFO)
-_UBUNTU_PKGINFO.update(_COMMON_PKGINFO)
-_DEBIAN_PKGINFO.update(_COMMON_PKGINFO)
-
-_PKGINFO = {
-    "fedora"  : _FEDORA_PKGINFO,
-    "centos"  : _FEDORA_PKGINFO,
-    "rhel"    : _FEDORA_PKGINFO,
-    "ubuntu"  : _UBUNTU_PKGINFO,
-    "debian"  : _DEBIAN_PKGINFO,
+_PKGINFO: Final[dict[str, dict[str, str]]] = {
+    "fedora": _FEDORA_PKGINFO,
+    "centos": _FEDORA_PKGINFO,
+    "rhel":   _FEDORA_PKGINFO,
+    "ubuntu": _UBUNTU_PKGINFO,
+    "debian": _DEBIAN_PKGINFO,
 }
 
 class ToolChecker(ClassHelpers.SimpleCloseContext):
@@ -87,14 +92,37 @@ class ToolChecker(ClassHelpers.SimpleCloseContext):
     meaningful suggestion if the OS package is not installed.
     """
 
-    def _read_os_release(self, sysroot="/"):
+    def __init__(self, pman: ProcessManagerType):
         """
-        Read and parse the '/etc/os-release' file, and return its contents as a dictionary.
+        Initialize a class instance.
+
+        Args:
+            pman: The process manager object that defines the host to check for the tools on.
         """
 
-        paths = ("/usr/lib/os-release", "/etc/os-release")
-        paths = [Path(sysroot) / path.lstrip("/") for path in paths]
-        osinfo = {}
+        self._pman = pman
+        self._osinfo: dict[str, str] = {}
+        # Tools name to tool path cache.
+        self._cache: dict[str, Path] = {}
+
+    def close(self):
+        """Uninitialize the class object."""
+        ClassHelpers.close(self, unref_attrs=("_pman",))
+
+    def _read_os_release(self, sysroot: str = "/") -> dict[str, str]:
+        """
+        Read and parse the '/etc/os-release' file, and return its contents as a dictionary.
+
+        Args:
+            sysroot: The system root directory to use when locating the os-release file.
+
+        Returns:
+            A dictionary containing the OS information from the os-release file.
+        """
+
+        path_strs = ("/usr/lib/os-release", "/etc/os-release")
+        paths = [Path(sysroot) / path.lstrip("/") for path in path_strs]
+        osinfo: dict[str, str] = {}
 
         for path in paths:
             with contextlib.suppress(Error):
@@ -108,7 +136,7 @@ class ToolChecker(ClassHelpers.SimpleCloseContext):
 
                         split_line = line.split("=")
                         if len(split_line) != 2:
-                            _LOG.warning("unexpected line in '%s'%s:\n%s\nExpected lines have have "
+                            _LOG.warning("Unexpected line in '%s'%s:\n%s\nExpected lines have have "
                                          "'key=value' format.", path, self._pman.hostmsg, line)
                             continue
 
@@ -118,19 +146,19 @@ class ToolChecker(ClassHelpers.SimpleCloseContext):
 
         if not osinfo:
             files = "\n".join([str(path) for path in paths])
-            raise ErrorNotFound(f"cannot discover OS information{self._pman.hostmsg}, these files "
+            raise ErrorNotFound(f"Cannot discover OS information{self._pman.hostmsg}, these files "
                                 f"were checked:\n{files}")
 
         return osinfo
 
-    def _get_osinfo(self):
+    def _get_osinfo(self) -> dict[str, str]:
         """Return the OS information dictionary."""
 
         if not self._osinfo:
             self._osinfo = self._read_os_release()
         return self._osinfo
 
-    def get_osname(self):
+    def get_osname(self) -> str:
         """
         Return name of the OS running on the target system. The name is read from the
         '/etc/os-release' file. Return '<unknown_OS_NAME>' if OS name could not be determined.
@@ -148,7 +176,7 @@ class ToolChecker(ClassHelpers.SimpleCloseContext):
             return osinfo["ID"].capitalize()
         return "<unknown_OS_NAME>"
 
-    def get_osid(self):
+    def get_osid(self) -> str:
         """
         Return ID of the OS running on the target system. The ID is read from the
         '/etc/os-release' file. Return '<unknown_OS_ID>' if OS ID could not be determined.
@@ -165,28 +193,37 @@ class ToolChecker(ClassHelpers.SimpleCloseContext):
 
         return "<unknown_OS_ID>"
 
-    def tool_to_pkg(self, tool, osid=None):
+    def tool_to_pkg(self, tool: str, osid: str = "") -> str:
         """
-        Get OS package name by tool name. The arguments are as follows.
-          * tool - name of the tool to get OS package name for.
-          * osid - optional OS ID (will be automatically discovered by default).
+        Get OS package name by tool name.
 
-        Return name of the OS package that provids 'tool'. Return 'None' if package name is was not
-        found.
+        Args:
+            tool: Name of the tool to get OS package name for.
+            osid: Optional OS ID (will be automatically discovered by default).
+
+        Returns:
+            Name of the OS package that provides 'tool'. Return an empty string if package name was
+            not found.
         """
 
         if not osid:
             osid = self.get_osid()
 
         if osid not in _PKGINFO:
-            return None
+            return ""
 
-        return _PKGINFO[osid].get(tool)
+        return _PKGINFO[osid].get(tool, "")
 
-    def check_tool(self, tool):
+    def check_tool(self, tool: str) -> Path:
         """
         Check if tool 'tool' is available on the target system. Return tool path if it is available,
         raise an 'ErrorNotFound' exception otherwise.
+
+        Args:
+            tool: Name of the tool to check for.
+
+        Returns:
+            Path to the tool if it is available.
         """
 
         if tool is self._cache:
@@ -197,7 +234,7 @@ class ToolChecker(ClassHelpers.SimpleCloseContext):
             self._cache[tool] = path
             return path
 
-        msg = f"failed to find tool '{tool}'{self._pman.hostmsg}"
+        msg = f"Failed to find tool '{tool}'{self._pman.hostmsg}"
 
         osid = self.get_osid()
 
@@ -207,18 +244,3 @@ class ToolChecker(ClassHelpers.SimpleCloseContext):
             msg += f".\nTry to install the '{osname}' OS package '{pkgname}'{self._pman.hostmsg}."
 
         raise ErrorNotFound(msg)
-
-    def __init__(self, pman):
-        """
-        The class constructor. The arguments are as follows.
-          * pman - the process manager object that defines the host to check for the tools on.
-        """
-
-        self._pman = pman
-        self._osinfo = None
-        # Tools name to tool path cache.
-        self._cache = {}
-
-    def close(self):
-        """Uninitialize the class object."""
-        ClassHelpers.close(self, unref_attrs=("_pman",))
